a21050e8b93132e5e933a971d14aa3b810ef4b03
samlerman <slerman@ur.rochester.edu>
2022-01-20 22:31:33 -0500

    debug

--- Blocks/Encoders.py
+++ Blocks/Encoders.py
@@ -34,10 +34,11 @@ class CNNEncoder(nn.Module):
         self.test = nn.Sequential(nn.Conv2d(self.in_channels, self.out_channels, 3, stride=2),
                                   nn.ReLU(),
                                   nn.Conv2d(self.out_channels, self.out_channels, 3, 1),
-                                  nn.ReLU(),
-                                  nn.Conv2d(self.out_channels, self.out_channels, 3, 1),
-                                  nn.ReLU(),
-                                  nn.Conv2d(self.out_channels, self.out_channels, 3, 1))
+                                  # nn.ReLU(),
+                                  # nn.Conv2d(self.out_channels, self.out_channels, 3, 1),
+                                  # nn.ReLU(),
+                                  # nn.Conv2d(self.out_channels, self.out_channels, 3, 1)
+                                  )
 
         # CNN
         self.CNN = nn.Sequential(*sum([(nn.Conv2d(self.in_channels if i == 0 else self.out_channels,
