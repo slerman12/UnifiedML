d827220ff740f6f4ed28aa5b515da82913cc2a23
samlerman <slerman@ur.rochester.edu>
2022-01-21 01:30:16 -0500

    testing dpg mean no sample and next obs nan uniform encoding vlog reel stack un-frame-stack appending reals

--- Datasets/Environment.py
+++ Datasets/Environment.py
@@ -72,7 +72,7 @@ class Environment:
                     if hasattr(self.env, 'physics') else self.env.render()
                 video_image.append(frame)
                 import torch
-                video_image.append(torch.tensor(exp.observation[:num_images]).to(action.device))
+                video_image.append(torch.tensor(exp.observation[:num_images]).to(action.device) / 127.5 - 1)
 
             # Tally reward, done, step
             self.episode_reward += exp.reward.mean()
