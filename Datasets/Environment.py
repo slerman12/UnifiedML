cf357ba126b4878807729b24f99748297593cd1f
samlerman <slerman@ur.rochester.edu>
2022-01-21 01:06:05 -0500

    testing dpg mean no sample and next obs nan uniform encoding

--- Datasets/Environment.py
+++ Datasets/Environment.py
@@ -65,7 +65,7 @@ class Environment:
             experiences.append(exp)
 
             if vlog or self.generate:
-                frame = action[0].view(exp.observation.shape[1:]) if self.generate \
+                frame = action.view(exp.observation.shape) if self.generate \
                     else self.env.physics.render(height=256, width=256, camera_id=0) \
                     if hasattr(self.env, 'physics') else self.env.render()
                 video_image.append(frame)
