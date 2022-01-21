3b3479f0532088d57716c9711a17a81462aac985
samlerman <slerman@ur.rochester.edu>
2022-01-20 22:33:53 -0500

    debug

--- Blocks/Encoders.py
+++ Blocks/Encoders.py
@@ -33,7 +33,7 @@ class CNNEncoder(nn.Module):
 
         self.test = nn.Sequential(nn.Conv2d(self.in_channels, self.out_channels, 3, stride=2),
                                   nn.ReLU(),
-                                  nn.Conv2d(self.out_channels, self.out_channels, 3, 2),
+                                  # nn.Conv2d(self.out_channels, self.out_channels, 3, 1),
                                   # nn.ReLU(),
                                   # nn.Conv2d(self.out_channels, self.out_channels, 3, 1),
                                   # nn.ReLU(),
