bc62137a37b5ea4e6486f95307400e9cf0133690
samlerman <slerman@ur.rochester.edu>
2022-01-24 00:18:20 -0500

    flat debug

--- repro.py
+++ repro.py
@@ -1,20 +1,18 @@
 import torch
 from torch import nn
 
-from Blocks.Encoders import CNNEncoder
-
 
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.CNN = nn.Sequential(nn.Conv2d(1, 32, 3, stride=2))
 
-    def forward(self, x, *context):
+    def forward(self, x):
         return self.CNN(x)
 
 
 # cnn = CNN()
-cnn = CNNEncoder((1, 28, 28)).to('cuda')
+cnn = CNN().to('cuda')
 x = torch.full([128, 1, 28, 28], float('nan')).to('cuda')
 print(x.shape, torch.isnan(x).all())
 y = cnn(x)
