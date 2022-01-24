02bba82b0c1d8f43ab4fcc97c3d9e60cdd8155ee
samlerman <slerman@ur.rochester.edu>
2022-01-23 23:05:15 -0500

    gan debug

--- Blocks/Encoders.py
+++ Blocks/Encoders.py
@@ -86,12 +86,12 @@ class CNNEncoder(nn.Module):
         # CNN encode
         print(obs.shape, (~torch.isnan(obs.flatten(1).sum(1))).any())
         h = self.CNN(obs)
+        print(h.shape, (~torch.isnan(h.flatten(1).sum(1))).any())
 
         h = h.view(*obs_shape[:-3], *h.shape[-3:])
         assert tuple(h.shape[-3:]) == self.repr_shape, 'pre-computed repr_shape does not match output CNN shape'
 
         if flatten:
-            print(h.flatten(-3).shape, (~torch.isnan(h.flatten(-3).flatten(1).sum(1))).any())
             return h.flatten(-3)
         return h
 
