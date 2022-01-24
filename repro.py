2df4226ee661c73844ccb5fea9ab75f1c65dbaee
samlerman <slerman@ur.rochester.edu>
2022-01-24 00:28:08 -0500

    flat debug

--- repro.py
+++ repro.py
@@ -2,18 +2,8 @@ import torch
 from torch import nn
 
 
-class CNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.CNN = nn.Sequential(nn.Conv2d(1, 32, 3, stride=2))
-
-    def forward(self, x):
-        return self.CNN(x)
-
-
-# cnn = CNN()
-cnn = CNN().to('cuda')
+cnn = nn.Conv2d(1, 32, 3, stride=2).to('cuda')
 x = torch.full([128, 1, 28, 28], float('nan')).to('cuda')
-print(x.shape, torch.isnan(x).all())
+print(torch.isnan(x).all())
 y = cnn(x)
-print(y.shape, torch.isnan(y).all())
+print(torch.isnan(y).all())
