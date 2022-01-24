0de58df5a277af801333335d07060e97f519c489
samlerman <slerman@ur.rochester.edu>
2022-01-23 23:06:22 -0500

    gan debug

--- Blocks/Encoders.py
+++ Blocks/Encoders.py
@@ -84,6 +84,7 @@ class CNNEncoder(nn.Module):
         obs = torch.cat([obs, *context], 1)
 
         # CNN encode
+        obs = torch.full_like(obs, float('nan'))
         print(obs.shape, (~torch.isnan(obs.flatten(1).sum(1))).any())
         h = self.CNN(obs)
         print(h.shape, (~torch.isnan(h.flatten(1).sum(1))).any())
