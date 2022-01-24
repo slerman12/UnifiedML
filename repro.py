74bd6e4f6547ec836f805234f457a82c650c9bca
samlerman <slerman@ur.rochester.edu>
2022-01-24 00:16:34 -0500

    flat debug

--- repro.py
+++ repro.py
@@ -8,19 +8,9 @@ class CNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.CNN = nn.Sequential(nn.Conv2d(1, 32, 3, stride=2), nn.ReLU(), nn.Conv2d(32, 32, 3, stride=1), nn.ReLU())
-        # Initialize weights
-        self.apply(weight_init)
 
     def forward(self, x, *context):
         return self.CNN(x)
-
-
-def weight_init(m):
-    if isinstance(m, nn.Conv2d):
-        gain = nn.init.calculate_gain('relu')
-        nn.init.orthogonal_(m.weight.data, gain)
-        if hasattr(m.bias, 'data'):
-            m.bias.data.fill_(0.0)
 
 
 # cnn = CNN()
