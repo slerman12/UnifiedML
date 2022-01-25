6ed1f367fd8ba3118e8e3dbdf6a71c54258a1c74
samlerman <slerman@ur.rochester.edu>
2022-01-25 00:03:43 -0500

    cleaned...

--- repro.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch import nn
-
-
-cnn = nn.Conv2d(1, 32, 3, stride=2).to('cuda')
-x = torch.full([128, 1, 28, 28], float('nan')).to('cuda')
-print(torch.isnan(x).all())
-y = cnn(x)
-print(torch.isnan(y).all())
