603d2c120bf053861c93281fd2a8322998757983
samlerman <slerman@ur.rochester.edu>
2022-02-14 17:26:15 -0500

    bionet cleaning...

--- Blocks/Architectures/LermanBlocks/BioNet/LocalityCNN.py
+++ Blocks/Architectures/LermanBlocks/BioNet/LocalityCNN.py
@@ -40,7 +40,7 @@ class Conv2DLocalized(nn.Module):
 
     def forward(self, input):
         x = self.conv(input)
-        x = einsum('b c h w, b h w d c, b h w d -> b c h w', x, self.linear_W, self.linear_B)
+        x = einsum('b c h w, h w d c, h w d -> b c h w', x, self.linear_W, self.linear_B)
         x = F.gelu(x)
         x = self.ln(x)
         return x
