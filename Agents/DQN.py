aeaed0c7b68a54a7f40996fa6c2fe4329908b8da
samlerman <slerman@ur.rochester.edu>
2022-02-15 15:18:49 -0500

    dqn rl classifier random debug

--- Agents/DQN.py
+++ Agents/DQN.py
@@ -142,7 +142,7 @@ class DQNAgent(torch.nn.Module):
             # Supervised learning
             if self.supervise:
                 actions = self.actor(obs[instruction], self.step).mean
-                labels = label[instruction].long().unsqueeze(1).expand_as(actions)
+                labels = label[instruction].long().unsqueeze(1).expand(*actions.shape[:-1])
 
                 # Supervised loss
                 supervised_loss = cross_entropy(actions, labels)
