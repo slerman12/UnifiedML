2979e0b2e8ed88c601150eb975abdaf19571f878
samlerman <slerman@ur.rochester.edu>
2022-02-17 06:54:33 -0500

    testing new recepes format

--- Blocks/Architectures/Vision/CNN.py
+++ Blocks/Architectures/Vision/CNN.py
@@ -35,20 +35,13 @@ class CNN(nn.Module):
         return Utils.cnn_feature_shape(h, w, self.CNN)
 
     def forward(self, *x):
-        print([bla.shape for bla in [context.view(*context.shape[:-3], -1, *self.input_shape[1:]) if len(context.shape) > 3
-                                    else context.view(*context.shape[:-1], -1, *self.input_shape[1:]) if math.prod(self.input_shape[1:])
-                                                                                                         % context.shape[-1] == 0
-        else context.view(*context.shape, 1, 1).expand(*context.shape, *self.input_shape[1:])
-                                    for context in x if context.nelement()]])
         # Concatenate inputs along channels assuming dimensions allow, broadcast across many possibilities
         x = torch.cat(
             [context.view(*context.shape[:-3], -1, *self.input_shape[1:]) if len(context.shape) > 3
-             else context.view(*context.shape[:-1], -1, *self.input_shape[1:]) if context.shape[-1] and
-                                                                                  math.prod(self.input_shape[1:])
+             else context.view(*context.shape[:-1], -1, *self.input_shape[1:]) if math.prod(self.input_shape[1:])
                                                                                   % context.shape[-1] == 0
              else context.view(*context.shape, 1, 1).expand(*context.shape, *self.input_shape[1:])
              for context in x if context.nelement() > 0], dim=-3)
-        print(x.shape)
         # Conserve leading dims
         lead_shape = x.shape[:-3]
         # Operate on last 3 dims
