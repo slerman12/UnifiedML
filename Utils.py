312ba78b56d8f9b1a1fdd55078cf36a07314565d
samlerman <slerman@ur.rochester.edu>
2022-02-17 09:31:50 -0500

    koila test

--- Utils.py
+++ Utils.py
@@ -251,7 +251,8 @@ class act_mode:
 from koila import lazy
 # Converts data to Torch Tensors and moves them to the specified device as floats
 def to_torch(xs, device):
-    return tuple([torch.as_tensor(x, device=device).float() for x in xs])
+    # return tuple(torch.as_tensor(x, device=device).float() for x in xs)
+    return lazy(*tuple(torch.as_tensor(x, device=device).float() for x in xs), batch=0)
 
 
 # Backward pass on a loss; clear the grads of models; update EMAs; step optimizers
