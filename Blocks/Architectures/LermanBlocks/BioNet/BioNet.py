70c88470f97c1ec774fc355ba7487a9c7f917ae6
samlerman <slerman@ur.rochester.edu>
2022-02-17 23:56:54 -0500

    more sym

--- Blocks/Architectures/LermanBlocks/BioNet/BioNet.py
+++ Blocks/Architectures/LermanBlocks/BioNet/BioNet.py
@@ -20,7 +20,7 @@ class BioNet(nn.Module):
         # self.ventral_stream = CNN(input_shape, out_channels, depth)
         # self.dorsal_stream = CNN(input_shape, out_channels, depth)
         self.ventral_stream = ResNet(input_shape, 3, 2, [64, 64, 128], [2, 2])
-        self.dorsal_stream = ResNet(input_shape, 3, 3, [64, 64, 128], [1, 1])
+        self.dorsal_stream = ResNet(input_shape, 3, 3, [64, 64, 128], [2, 2])
 
         dims = self.ventral_stream.dims[1:]
 
