4c0d5b020257ca0275822c85be7835691da3710f
samlerman <slerman@ur.rochester.edu>
2022-02-18 22:44:25 -0500

    attn efficiency

--- Blocks/Architectures/MultiHeadAttention.py
+++ Blocks/Architectures/MultiHeadAttention.py
@@ -48,7 +48,7 @@ class CrossAttention(nn.Module):
         q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), (q, k, v))
 
         # Memory efficient toggle, e.g., =0.5
-        mem_limit = 0.3
+        mem_limit = 0.2
         einsum = torch.einsum if mem_limit else EinsumPlanner(q.device, cuda_mem_limit=mem_limit).einsum
 
         dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.dim ** -0.5
