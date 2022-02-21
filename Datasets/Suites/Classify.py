8a764cd48aa62fc7cdb160b8748909c9aadef5f6
samlerman <slerman@ur.rochester.edu>
2022-02-21 09:54:25 -0500

    classify desc for loading batches

--- Datasets/Suites/Classify.py
+++ Datasets/Suites/Classify.py
@@ -63,8 +63,8 @@ class ClassifyEnv:
         path.mkdir(exist_ok=True, parents=True)
 
         for episode_ind, (x, y) in enumerate(tqdm(self.batches, 'Creating experience replay for this dataset. '
-                                                                'This only has to be done once.'
-                                                                'Loading in batches.')):
+                                                                'This only has to be done once... '
+                                                                'loading in batches')):
             x, y, dummy_action, dummy_reward, dummy_discount, dummy_step = self.reset_format(x, y)
 
             # Concat a dummy batch item
