17ad15c9949d2f5e23b932e83999eb306265a0c7
samlerman <slerman@ur.rochester.edu>
2022-02-22 13:57:25 -0500

    replay message

--- Datasets/ExperienceReplay.py
+++ Datasets/ExperienceReplay.py
@@ -31,11 +31,12 @@ class ExperienceReplay:
                     exists = [f'./Datasets/ReplayBuffer/Classify/{task}_Buffer/']
                     print('All data loaded. Training of classifier underway.')
                 else:
-                    warnings.warn(f'Loading a saved replay of a classify task from a previous online session. '
-                                  f'For the standard offline dataset, set '  # Assuming one exists
-                                  f'replay.path="./Datasets/ReplayBuffer/Classify/{task}_Buffer" ' 
-                                  f'or delete the saved buffer in {path.replace("Agents.", "")}.')
-            assert len(exists) > 0, f'No existing replay buffer found in path: {path.replace("Agents.", "")}'
+                    if path != f'./Datasets/ReplayBuffer/Classify/{task}_Buffer':
+                        warnings.warn(f'Loading a saved replay of a classify task from a previous online session. '
+                                      f'For the standard offline dataset, set '  # Assuming one exists
+                                      f'replay.path="./Datasets/ReplayBuffer/Classify/{task}_Buffer" ' 
+                                      f'or delete the saved buffer in {path.replace("Agents.", "")}.')
+                assert len(exists) > 0, f'No existing replay buffer found in path: {path.replace("Agents.", "")}'
             self.path = Path(sorted(exists)[-1])
             save = offline or generate or save
         else:
