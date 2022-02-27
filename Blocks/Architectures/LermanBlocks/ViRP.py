1ebb66a4e86099d768dbfaf3226dba0fad6b0941
samlerman <slerman@ur.rochester.edu>
2022-02-26 21:29:11 -0500

    virp-relative

--- Blocks/Architectures/LermanBlocks/ViRP.py
+++ Blocks/Architectures/LermanBlocks/ViRP.py
@@ -3,7 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # MIT_LICENSE file in the root directory of this source tree.
 from Blocks.Architectures.LermanBlocks.MultiHeadRelation import *
-from Blocks.Architectures.MultiHeadAttention import CrossAttentionBlock
 
 
 class ViRP(ViT):
--- Blocks/Architectures/__init__.py
+++ Blocks/Architectures/__init__.py
@@ -10,6 +10,7 @@ from Blocks.Architectures.Vision.ResNet import MiniResNet, ResNet18, ResNet50
 from Blocks.Architectures.Vision.ResNet import MiniResNet as ResNet
 from Blocks.Architectures.Vision.ConvMixer import ConvMixer
 from Blocks.Architectures.Vision.ConvNeXt import ConvNeXt
+from Blocks.Architectures.LermanBlocks.ViRP import ViRP
 
 
 from torch import nn
