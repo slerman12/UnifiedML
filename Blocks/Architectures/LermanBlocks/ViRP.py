c8927908a6b2c51cb86ec68c7d6856da5bcc0c57
samlerman <slerman@ur.rochester.edu>
2022-02-26 22:06:25 -0500

    virp-relative

--- Blocks/Architectures/LermanBlocks/ViRP.py
+++ Blocks/Architectures/LermanBlocks/ViRP.py
@@ -2,7 +2,15 @@
 #
 # This source code is licensed under the MIT license found in the
 # MIT_LICENSE file in the root directory of this source tree.
-from Blocks.Architectures.LermanBlocks.MultiHeadRelation import *
+import math
+
+import torch
+from torch import nn
+
+from Blocks.Architectures import MLP
+from Blocks.Architectures.MultiHeadAttention import ReLA
+from Blocks.Architectures.RN import RN
+from Blocks.Architectures.Vision.ViT import ViT
 
 
 class ViRP(ViT):
@@ -10,12 +18,10 @@ class ViRP(ViT):
                  experiment='head_head_in_RN'):
         super().__init__(input_shape, patch_size, out_channels, heads, depth, pool, True, output_dim)
 
-        if experiment == 'concat_plus_mid':
-            core = RelationSimplest  # not important
-        elif experiment == 'concat_plus_in':  # velocity reasoning from mlp only
-            core = RelationSimplestV2
+        if experiment == 'concat_plus_in':  # velocity reasoning from mlp only
+            core = RelationConcat
         elif experiment == 'plus_in_concat_plus_mid':  # see if attention is useful as "reason-er"
-            core = RelationSimplestV3
+            core = RelationConcatV2
         elif experiment == 'head_wise_ln':  # disentangled relational reasoning - are the heads independent?
             core = RelationDisentangled
         elif experiment == 'head_in_RN':  # invariant relational reasoning between input-head - are they?
@@ -30,4 +36,121 @@ class ViRP(ViT):
             core = RelationRelative
 
         self.attn = nn.Sequential(*[core(out_channels, heads) for _ in range(depth)])
+
+
+# Concat, then residual from input
+class RelationConcat(nn.Module):
+    def __init__(self, dim=32, heads=8, context_dim=None, value_dim=None):
+        super().__init__()
+
+        context_dim = dim if context_dim is None \
+            else context_dim
+
+        value_dim = dim if value_dim is None \
+            else value_dim
+
+        self.heads = math.gcd(8, value_dim) if heads is None \
+            else heads
+
+        self.value_dim = value_dim
+
+        self.attn = ReLA(dim, self.heads, context_dim, value_dim)
+        self.mlp = MLP(value_dim + dim, value_dim, value_dim, 1, nn.GELU())
+
+        self.LN_mid = nn.LayerNorm(value_dim)
+        self.LN_out = nn.LayerNorm(value_dim)
+
+    def repr_shape(self, c, h, w):
+        return self.value_dim, h, w  # Assumes channels last
+
+    def forward(self, x, context=None):
+        if context is None:
+            context = x
+
+        attn = self.LN_mid(self.attn(x, context))  # Relation
+        out = self.LN_out(self.mlp(attn, x)) + x  # Reason-er
+
+        return out
+
+
+# Input to middle residual, concat, then middle to out residual
+class RelationConcatV2(RelationConcat):
+    def forward(self, x, context=None):
+        if context is None:
+            context = x
+
+        attn = self.LN_mid(self.attn(x, context)) + x  # Attention = Reason-er
+        out = self.LN_out(self.mlp(attn, x)) + attn  # MLP = Course Corrector
+
+        return out
+
+
+# Heads layer norm'd
+class RelationDisentangled(RelationConcat):
+    def __init__(self, dim=32, heads=8, context_dim=None, value_dim=None):
+        super().__init__(dim, heads, context_dim, value_dim)
+
+        self.LN_mid = nn.LayerNorm(self.value_dim / self.heads)
+
+    def forward(self, x, context=None):
+        if context is None:
+            context = x
+
+        attn = self.attn(x, context)
+        head_wise = attn.view(*attn.shape[:-1], self.heads, -1)
+        norm = self.LN_mid(head_wise)
+        disentangled = norm.view(attn.shape)
+
+        out = self.LN_out(self.mlp(disentangled, x)) + x
+
+        return out
+
+
+class RelationSimpler(RelationDisentangled):
+    def __init__(self, dim=32, heads=1, context_dim=None, value_dim=None):
+        super().__init__(dim, heads, context_dim, value_dim)
+
+        self.RN = RN(dim, dim)
+
+    def forward(self, x, context=None):
+        if context is None:
+            context = x  # [b, n, d]
+
+        attn = self.attn(x, context)  # [b, n, h * d]
+        head_wise = attn.view(*attn.shape[:-1], self.heads, -1)  # [b, n, h, d]
+
+        norm = self.LN_mid(head_wise)  # [b, n, h, d]
+        residual = x.unsqueeze(-2)  # [b, n, 1, d]
+
+        relation = norm.flatten(0, -2)  # [b * n, h, d]
+        residual = residual.flatten(0, -2)  # [b * n, 1, d]
+
+        out = self.LN_out(self.RN(relation, residual))  # [b * n, d]
+
+        return out.view(x.shape) + x  # [b, n, d]
+
+
+class RelationRelative(RelationDisentangled):
+    def __init__(self, dim=32, heads=1, context_dim=None, value_dim=None):
+        super().__init__(dim, heads, context_dim, value_dim)
+
+        self.RN = RN(dim, dim * 2)
+
+    def forward(self, x, context=None):
+        if context is None:
+            context = x  # [b, n, d]
+
+        attn = self.attn(x, context)  # [b, n, h * d]
+        head_wise = attn.view(*attn.shape[:-1], self.heads, -1)  # [b, n, h, d]
+
+        norm = self.LN_mid(head_wise)  # [b, n, h, d]
+        residual = x.unsqueeze(-2)  # [b, n, 1, d]
+
+        relation = norm.flatten(0, -2)  # [b * n, h, d]
+        residual = residual.flatten(0, -2)  # [b * n, 1, d]
+        context = torch.cat([residual.expand_as(relation), residual], -1)  # [b * n, h, d * 2]
+
+        out = self.LN_out(self.RN(relation, context))  # [b * n, d]
+
+        return out.view(x.shape) + x  # [b, n, d]
 
