e5d82bf012a86f19a86d0622dd17c4cf481ce2fa
samlerman <slerman@ur.rochester.edu>
2022-02-27 21:06:12 -0500

    ViRP

--- Blocks/Architectures/MultiHeadAttention.py
+++ Blocks/Architectures/MultiHeadAttention.py
@@ -52,8 +52,6 @@ class CrossAttention(nn.Module):
         if context is None:
             context = x
 
-        print(x.shape, context.shape)
-
         tokens = len(x.shape) == 2
         if not tokens:
             x = x.flatten(1, -2)
