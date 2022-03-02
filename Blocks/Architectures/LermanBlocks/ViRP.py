a9f21d0a37b6ca7bad9f99cc07c42ab53bc3cd73
samlerman <slerman@ur.rochester.edu>
2022-03-01 22:57:55 -0500

    toke relation expent

--- Blocks/Architectures/LermanBlocks/ViRP.py
+++ Blocks/Architectures/LermanBlocks/ViRP.py
@@ -27,7 +27,7 @@ class ViRP(ViT):
 
         self.ViRP = ViRP
         if ViRP:
-            self.tokens_per_axis = 20
+            self.tokens_per_axis = 50
 
         super().__init__(input_shape, patch_size, out_channels, heads, depth, pool, True, output_dim)
 
