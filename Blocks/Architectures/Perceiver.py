317470b6c284be596c9fe8d33f913186a6a25adf
samlerman <slerman@ur.rochester.edu>
2022-03-03 23:09:41 -0500

    mlp perceiver

--- Blocks/Architectures/Perceiver.py
+++ Blocks/Architectures/Perceiver.py
@@ -8,11 +8,12 @@ import torch
 from torch import nn
 from torch.nn import init
 
+from Blocks.Architectures import MLP
 from Blocks.Architectures.MultiHeadAttention import CrossAttentionBlock
 
 
 class Perceiver(nn.Module):
-    def __init__(self, dim, heads=8, tokens=32, token_dim=None, value_dim=None, depth=3, relu=False):
+    def __init__(self, dim, heads=8, tokens=32, token_dim=None, value_dim=None, depth=3, relu=False, mlp_depth=None):
         super().__init__()
 
         token_dim = dim if token_dim is None else token_dim
@@ -21,11 +22,14 @@ class Perceiver(nn.Module):
         self.tokens = nn.Parameter(torch.randn(tokens, token_dim))
         init.kaiming_uniform_(self.tokens, a=math.sqrt(5))
 
+        self.mlp = nn.Identity() if mlp_depth is None \
+            else nn.Sequential(MLP(dim, dim, dim, mlp_depth), nn.ReLU(inplace=True))
         self.attn_token = CrossAttentionBlock(token_dim, heads, dim, value_dim, relu=relu)
         self.reattn_token = CrossAttentionBlock(value_dim, heads, dim, value_dim, relu=relu)
         self.attn = nn.Sequential(*[CrossAttentionBlock(value_dim, heads, relu=relu) for _ in range(depth - 1)])
 
     def forward(self, x):
+        x = self.mlp(x)
         tokens = self.attn_token(self.tokens, x)
         x = self.reattn_token(tokens, x)
         return self.attn(x)
