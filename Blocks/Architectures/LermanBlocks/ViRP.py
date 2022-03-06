6afc9c492bba699f772bde9b6c330d66e21a841d
samlerman <slerman@ur.rochester.edu>
2022-03-05 20:49:42 -0500

    virp fix token pre blocks 2

--- Blocks/Architectures/LermanBlocks/ViRP.py
+++ Blocks/Architectures/LermanBlocks/ViRP.py
@@ -20,7 +20,7 @@ from Blocks.Architectures.Perceiver import Perceiver, PerceiverV2
 
 class ViRPV2(ViT):
     def __init__(self, input_shape, patch_size=4, out_channels=128, emb_dropout=0, tokens=20, token_dim=128,
-                 qk_dim=None, v_dim=None, hidden_dim=None, heads=8, pre_blocks=3, depths=[8], recursions=None,
+                 qk_dim=None, v_dim=None, hidden_dim=None, heads=8, pre_blocks=2, depths=[8], recursions=None,
                  pool='cls', output_dim=None, experiment='relation', ViRS=False):
         self.tokens = tokens
         self.ViRS = ViRS
