a96ed4f16b383a60b55e12966c7c646a033604b5
samlerman <slerman@ur.rochester.edu>
2022-03-17 21:37:06 -0400

    dqn debug...

--- Losses/QLearning.py
+++ Losses/QLearning.py
@@ -46,7 +46,7 @@ def ensembleQLearning(critic, actor, obs, action, reward, discount, next_obs, st
             next_q_logits = next_q - next_q.max(dim=-1, keepdim=True)[0]
             next_probs = torch.softmax(next_q_logits + next_actions_log_probs, -1)
             next_v[has_future] = torch.sum(next_q * next_probs, -1, keepdim=True)
-            print(next_v.shape)
+            print(next_v.shape, (next_probs == 1).all())
 
             target_q += discount * next_v
 
