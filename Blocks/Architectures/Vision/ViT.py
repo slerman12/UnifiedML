abb9e6b4246a960f6952f0df850833cb6ea544c2
samlerman <slerman@ur.rochester.edu>
2022-03-17 22:20:23 -0400

    reverted pooling

--- Blocks/Architectures/Vision/ViT.py
+++ Blocks/Architectures/Vision/ViT.py
@@ -47,8 +47,10 @@ class ViT(nn.Module):
         self.attn = nn.Sequential(*[SelfAttentionBlock(out_channels, heads, out_channels, qk_dim, v_dim, hidden_dim,
                                                        dropout=dropout, relu=relu) for _ in range(depth)])
 
-        self.pool = nn.Sequential(Pool(pool_type),
-                                  nn.Identity() if output_dim is None else nn.Linear(out_channels, output_dim))
+        self.pool = nn.Identity() if output_dim is None \
+            else nn.Sequential(Pool(pool_type),
+                               nn.LayerNorm(out_channels),
+                               MLP(out_channels, output_dim, 1024))
 
     def repr_shape(self, c, h, w):
         return (self.out_channels, 1, (h // self.patch_size) * (w // self.patch_size) + 1) if self.output_dim is None \
@@ -79,9 +81,7 @@ class ViT(nn.Module):
         x = self.attn(x)
 
         x = rearrange(x, 'b (h w) c -> b c h w', h=self.h, w=self.w)  # Channels 1st
-
-        if self.output_dim is not None:
-            x = self.pool(x)
+        x = self.pool(x)
 
         # Restore leading dims
         out = x.view(*lead_shape, *x.shape[1:])
