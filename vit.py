51ed2d52476ce17c999e412138eac34b6377cec5
samlerman <slerman@ur.rochester.edu>
2022-03-18 21:17:39 -0400

    replay repro test with norm

--- vit.py
+++ vit.py
@@ -304,11 +304,12 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True,
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
 
-# obs_spec = {'name': 'obs', 'shape': (3,32,32), 'dtype': 'float32'}
-# action_spec = {'name': 'action', 'shape': (10,), 'dtype': 'float32'}
-# trainloader = ExperienceReplay(bs, 8, 10000000, action_spec, 'classify', 'CIFAR10', True, False, True, True,
-#                                './Datasets/ReplayBuffer/Classify/CIFAR10_Buffer', obs_spec, 0, 1,
-#                                {'RandomCrop': {'size': 32, 'padding': 4}, 'RandomHorizontalFlip': {}})
+obs_spec = {'name': 'obs', 'shape': (3,32,32), 'dtype': 'float32'}
+action_spec = {'name': 'action', 'shape': (10,), 'dtype': 'float32'}
+trainloader = ExperienceReplay(bs, 8, 10000000, action_spec, 'classify', 'CIFAR10', True, False, True, True,
+                               './Datasets/ReplayBuffer/Classify/CIFAR10_Buffer', obs_spec, 0, 1,
+                               {'RandomCrop': {'size': 32, 'padding': 4}, 'RandomHorizontalFlip': {}, 'ToTensor': {},
+                                'Normalize': {'mean': (0.4914, 0.4822, 0.4465), 'std': (0.2023, 0.1994, 0.2010)}})
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -388,10 +389,9 @@ def train(epoch):
     train_loss = 0
     correct = 0
     total = 0
-    # for batch_idx, (inputs, _, _, _, _, targets, _, _, _, _, _) in enumerate(trainloader):
-    #     inputs, targets = inputs.to(device), targets.to(device).long()
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        inputs, targets = inputs.to(device), targets.to(device)
+    for batch_idx, (inputs, _, _, _, _, targets, _, _, _, _, _) in enumerate(trainloader):
+    # for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device).long()
         # Train with amp
         # with torch.cuda.amp.autocast(enabled=use_amp):
         outputs = net(inputs)
