2f06ab6609beb76a9f4c42901c7dd7126ae48f5e
samlerman <slerman@ur.rochester.edu>
2022-03-18 20:25:12 -0400

    repro replay test

--- vit.py
+++ vit.py
@@ -408,7 +408,7 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, 50000, 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
     return train_loss/(batch_idx+1)
 
