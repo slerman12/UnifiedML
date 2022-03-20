ab581835443158eba9aa15ee6132e6761b5f6a8d
samlerman <slerman@ur.rochester.edu>
2022-03-20 00:08:45 -0400

    test plot

--- Plot.py
+++ Plot.py
@@ -78,8 +78,6 @@ def plot(path, plot_experiments=None, plot_agents=None, plot_suites=None, plot_t
 
         datums = [experiment, suite.lower(), suite_task, agent]
         for i, spec in enumerate(specs):
-            if spec is not None:
-                print(datums[i], '(?:% s)' % '|'.join(spec), re.match('(?:% s)' % '|'.join(spec), datums[i]))
             if spec is not None and not re.match('(?:% s)' % '|'.join(spec), datums[i]):
                 include = False
 
