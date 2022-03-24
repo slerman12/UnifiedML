b4350e66ba7895530f10f388c841eebcd934d53d
samlerman <slerman@ur.rochester.edu>
2022-03-23 22:13:34 -0400

    loading waits

--- Utils.py
+++ Utils.py
@@ -42,7 +42,7 @@ def load(path, device, attr=None, persevere=False):
     except Exception as e:  # Pytorch's load and save are not atomic transactions
         if persevere:
             warnings.warn(f'Load conflict, resolving...')  # For distributed training
-            time.sleep(0.01)
+            time.sleep(0.1)
             return load(path, device, attr, True)
         else:
             assert Path(path).exists(), f'Load path {path} does not exist.'
