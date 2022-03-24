85bedb36f776fd5e104b2bcf7235d3ffe484d7da
samlerman <slerman@ur.rochester.edu>
2022-03-24 15:03:00 -0400

    DPG

--- Agents/Lermanbots/DPG.py
+++ Agents/Lermanbots/DPG.py
@@ -79,7 +79,7 @@ class DPGAgent(torch.nn.Module):
             Pi = actor(obs, self.step)
 
             if self.discrete:
-                Pi = self.action_selector(Pi, self.step, action=torch.arange(self.action_dim))
+                Pi = self.action_selector(Pi, self.step, action=torch.arange(self.action_dim, device=self.device))
 
             action = Pi.sample() if self.training \
                 else Pi.best if self.discrete \
