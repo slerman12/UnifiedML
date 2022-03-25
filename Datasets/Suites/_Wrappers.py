6a54f5d0922f93102d65729835fd3705e0df5641
samlerman <slerman@ur.rochester.edu>
2022-03-25 04:44:02 -0400

    DPG maybe working

--- Datasets/Suites/_Wrappers.py
+++ Datasets/Suites/_Wrappers.py
@@ -342,7 +342,7 @@ class DiscreteEnvWrapper(dm_env.Environment):
         if len(action.shape) and action.shape[-1] > 1:
             # Discretize
             if self.train:
-                action = action.exp()
+                action = np.exp(action)
                 action = np.random.choice(action.shape[-1], p=action / action.sum())
             else:
                 action = np.argmax(action, -1)
