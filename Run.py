bb9cd671ce083b47817830f58a1e1cf70f8f2ac6
samlerman <slerman@ur.rochester.edu>
2022-04-21 19:29:28 -0400

    deprecation warbing?

--- Run.py
+++ Run.py
@@ -15,53 +15,53 @@ torch.backends.cudnn.benchmark = True
 # Hyper-param arg files located in ./Hyperparams
 
 @hydra.main(config_path='Hyperparams', config_name='args')
-def main(args):
+def main(arg):
     # Set seeds
-    Utils.set_seeds(args.seed)
+    Utils.set_seeds(arg.seed)
 
-    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    arg.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # Train, test environments
-    env = instantiate(args.environment)
-    generalize = instantiate(args.environment, train=False, seed=args.seed + 1234)
+    env = instantiate(arg.environment)
+    generalize = instantiate(arg.environment, train=False, seed=arg.seed + 1234)
 
     for arg in ('obs_shape', 'action_shape', 'discrete', 'obs_spec', 'action_spec', 'evaluate_episodes', 'data_norm'):
         if hasattr(generalize, arg):
-            setattr(args, arg, getattr(generalize, arg))
+            setattr(arg, arg, getattr(generalize, arg))
 
     # Agent
-    agent = Utils.load(args.save_path, args.device) if args.load \
-        else instantiate(args.agent).to(args.device)
+    agent = Utils.load(arg.save_path, arg.device) if arg.load \
+        else instantiate(arg.agent).to(arg.device)
 
-    args.train_steps += agent.step
+    arg.train_steps += agent.step
 
     # Experience replay
-    replay = instantiate(args.replay)
+    replay = instantiate(arg.replay)
 
     # Loggers
-    logger = instantiate(args.logger)
+    logger = instantiate(arg.logger)
 
-    vlogger = instantiate(args.vlogger)
+    vlogger = instantiate(arg.vlogger)
 
     # Start
     converged = training = False
     while True:
         # Evaluate
-        if args.evaluate_per_steps and agent.step % args.evaluate_per_steps == 0:
+        if arg.evaluate_per_steps and agent.step % arg.evaluate_per_steps == 0:
 
-            for ep in range(args.evaluate_episodes):
+            for ep in range(arg.evaluate_episodes):
                 _, logs, vlogs = generalize.rollout(agent.eval(),  # agent.eval() just sets agent.training to False
-                                                    vlog=args.log_video)
+                                                    vlog=arg.log_video)
 
                 logger.log(logs, 'Eval')
 
             logger.dump_logs('Eval')
 
-            if args.log_video:
+            if arg.log_video:
                 vlogger.dump_vlogs(vlogs, f'{agent.step}')
 
-        if args.plot_per_steps and agent.step % args.plot_per_steps == 0:
-            call(args.plotting)
+        if arg.plot_per_steps and agent.step % arg.plot_per_steps == 0:
+            call(arg.plotting)
 
         if converged:
             break
@@ -72,28 +72,28 @@ def main(args):
         replay.add(experiences)
 
         if env.episode_done:
-            if args.log_per_episodes and agent.episode % args.log_per_episodes == 0:
+            if arg.log_per_episodes and agent.episode % arg.log_per_episodes == 0:
                 logger.log(logs, 'Train' if training else 'Seed', dump=True)
 
-            if env.last_episode_len > args.nstep:
+            if env.last_episode_len > arg.nstep:
                 replay.add(store=True)  # Only store full episodes
 
-        converged = agent.step >= args.train_steps
-        training = training or (agent.step > args.seed_steps or env.offline) and len(replay) >= args.num_workers
+        converged = agent.step >= arg.train_steps
+        training = training or (agent.step > arg.seed_steps or env.offline) and len(replay) >= arg.num_workers
 
         # Train agent
-        if training and args.learn_per_steps and agent.step % args.learn_per_steps == 0 or converged:
+        if training and arg.learn_per_steps and agent.step % arg.learn_per_steps == 0 or converged:
 
-            for _ in range(args.learn_steps_after if converged else 1):  # Additional updates after all rollouts
+            for _ in range(arg.learn_steps_after if converged else 1):  # Additional updates after all rollouts
                 logs = agent.train().learn(replay)  # Trains the agent
-                if args.log_per_episodes and agent.episode % args.log_per_episodes == 0:
+                if arg.log_per_episodes and agent.episode % arg.log_per_episodes == 0:
                     logger.log(logs, 'Train')
 
-        if training and args.save_per_steps and agent.step % args.save_per_steps == 0 or (converged and args.save):
-            Utils.save(args.save_path, agent, args.agent, 'step', 'episode')
+        if training and arg.save_per_steps and agent.step % arg.save_per_steps == 0 or (converged and arg.save):
+            Utils.save(arg.save_path, agent, arg.agent, 'step', 'episode')
 
-        if training and args.load_per_steps and agent.step % args.load_per_steps == 0:
-            agent = Utils.load(args.save_path, args.device, agent, preserve=['step', 'episode'], distributed=True)
+        if training and arg.load_per_steps and agent.step % arg.load_per_steps == 0:
+            agent = Utils.load(arg.save_path, arg.device, agent, preserve=['step', 'episode'], distributed=True)
 
 
 if __name__ == '__main__':
