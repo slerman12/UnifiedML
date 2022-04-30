57f223dd91ff1163a4cdef7fac8e6e0ff22f96a3
samlerman <slerman@ur.rochester.edu>
2022-04-30 15:15:40 -0400

    hydra plot

--- Plot.py
+++ Plot.py
@@ -366,7 +366,7 @@ def main(args):
         args.plotting.path = f"./Benchmarking/{'_'.join(args.plotting.plot_experiments)}/Plots"
     if 'steps' not in sys_args:
         args.plotting.steps = np.inf
-    print(**args.plotting)
+    print(args.plotting.path)
     plot(**args.plotting)
 
 
