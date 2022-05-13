2270e8bbb698c1c6d0ec1d5d58ad67f38267a851
samlerman <slerman@ur.rochester.edu>
2022-05-13 14:25:17 -0400

    plot styles

--- Plot.py
+++ Plot.py
@@ -56,7 +56,7 @@ def plot(path, plot_experiments=None, plot_agents=None, plot_suites=None, plot_t
                   # palette='Set2',
                   rc={
                       'legend.loc': 'lower right', 'figure.dpi': 400,
-                      'legend.fontsize': 7, 'legend.title_fontsize': 7,
+                      'legend.fontsize': 4.5, 'legend.title_fontsize': 4.5,
                       # 'axes.titlesize': 4, 'axes.labelsize': 4, 'font.size': 4,
                       # 'xtick.labelsize': 7, 'ytick.labelsize': 7,
                       # 'figure.titlesize': 4
@@ -206,6 +206,8 @@ def plot(path, plot_experiments=None, plot_agents=None, plot_suites=None, plot_t
             ax.yaxis.set_major_formatter(FuncFormatter('{:.0%}'.format))
             ax.set_ylabel('Eval Accuracy')
 
+        ax.legend().set_title(None)
+
     plt.tight_layout()
     plt.savefig(path / (plot_name + 'Tasks.png'))
 
@@ -262,6 +264,8 @@ def plot(path, plot_experiments=None, plot_agents=None, plot_suites=None, plot_t
             ax.set_ybound(0, 1)
             ax.yaxis.set_major_formatter(FuncFormatter('{:.0%}'.format))
             ax.set_ylabel('Eval Accuracy')
+
+        ax.legend().set_title(None)
 
     plt.tight_layout()
     plt.savefig(path / (plot_name + 'Suites.png'))
@@ -338,7 +342,7 @@ def plot(path, plot_experiments=None, plot_agents=None, plot_suites=None, plot_t
 
             ax.set_xticklabels(ax.get_xticklabels(), rotation=20)
             ax.set(xlabel=None)
-            sns.move_legend(ax, "lower right", bbox_to_anchor=(1.05, 1), borderaxespad=0.)
+            ax.legend().set_title(None)
 
         plt.tight_layout()
         plt.savefig(path / (plot_name + 'Bar.png'))
