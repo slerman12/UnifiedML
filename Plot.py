41d8795e6e9b6a5336933888a8e779d8b56bbd67
samlerman <slerman@ur.rochester.edu>
2022-05-13 20:24:04 -0400

    plot debug

--- Plot.py
+++ Plot.py
@@ -163,6 +163,12 @@ def plot(path, plot_experiments=None, plot_agents=None, plot_suites=None, plot_t
         if steps < np.inf:
             task_data = task_data[task_data['Step'] <= steps]
 
+        # No need to show Agent in legend if all same
+        if len(task_data.Agent.str.split('(').str[0].unique()) == 1:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", category=SettingWithCopyWarning)
+                task_data['Agent'] = task_data.Agent.str.split('(').str[1:].str.join('(').str.split(')').str[:-1].str.join(')')
+
         row = i // num_cols
         col = i % num_cols
         ax = axs[row, col] if num_rows > 1 and num_cols > 1 else axs[col] if num_cols > 1 \
@@ -194,17 +200,9 @@ def plot(path, plot_experiments=None, plot_agents=None, plot_suites=None, plot_t
                         tabular_normalized_median[agent][suite][task] = normalized.median()
                         break
 
-        # No need to show Agent in legend if all same
-        short_palette = palette
-        if len(task_data.Agent.str.split('(').str[0].unique()) == 1:
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=SettingWithCopyWarning)
-                task_data['Agent'] = task_data.Agent.str.split('(').str[1:].str.join('(').str.split(')').str[:-1].str.join(')')
-                short_palette = {')'.join('('.join(agent.split('(')[1:]).split(')')[:-1]): palette[agent] for agent in palette}
-
         hue_order = np.sort(task_data.Agent.unique())
         sns.lineplot(x='Step', y=y_axis, data=task_data, ci='sd', hue='Agent', hue_order=hue_order, ax=ax,
-                     palette=short_palette
+                     palette=palette
                      )
         ax.set_title(f'{title}')
 
@@ -341,13 +339,8 @@ def plot(path, plot_experiments=None, plot_agents=None, plot_suites=None, plot_t
                     bar_data[suite]['Median'].append(median[agent][suite][task])
                     bar_data[suite]['Agent'].append(agent)
 
-        # Max agents for a task
-        max_agents = max([len(set([bar_data[suite]['Agent'][i] for i, _ in enumerate(bar_data[suite]['Agent'])
-                                   if bar_data[suite]['Task'][i] == task])) for suite in bar_data
-                          for task in set(bar_data[suite]['Task'])])
-
         # Create subplots
-        fig, axs = plt.subplots(1, num_cols, figsize=(1.5 * max(max_agents, 3) * num_cols, 3))
+        fig, axs = plt.subplots(1, num_cols, figsize=(5.5 * num_cols, 3))
 
         for col, suite in enumerate(bar_data):
             task_data = pd.DataFrame(bar_data[suite])
