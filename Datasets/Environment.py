cfa02c3b4d8ce5bebf143c1228ec7828e3e59656
samlerman <agi.init@gmail.com>
2022-06-15 04:06:53 -0400

    never mind!

--- Datasets/Environment.py
+++ Datasets/Environment.py
@@ -58,10 +58,10 @@ class Environment:
             experiences.append(exp)
 
             if vlog or self.generate:
-                image = action[:24].view(-1, *exp.observation.shape[1:]) if self.generate \
+                image_frame = action[:24].view(-1, *exp.observation.shape[1:]) if self.generate \
                     else self.env.physics.render(height=256, width=256, camera_id=0) \
                     if hasattr(self.env, 'physics') else self.env.render()
-                video_image.append(image)
+                video_image.append(image_frame)
 
             # Tally reward, done
             self.episode_reward += exp.reward.mean()
