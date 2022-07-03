063973a68105cb65785315e7327a97428fb88a15
samlerman <agi.init@gmail.com>
2022-07-03 09:37:36 -0400

    utils load debug

--- Utils.py
+++ Utils.py
@@ -79,7 +79,7 @@ def load(path, device, model=None, preserve=(), distributed=False, attr='', **kw
             setattr(model, key, to_load[key])
 
     # Can also load part of a model. Useful for recipes,
-    # e.g. python Run.py Eyes=load +eyes.path=<checkpoint> +eyes.attr=encoder.Eyes +eyes.device=${device}
+    # e.g. python Run.py Eyes=load +eyes.path=<checkpoint> +eyes.attr=encoder.Eyes +eyes.device='cuda'
     for key in attr.split('.'):
         if key:
             model = getattr(model, key)
