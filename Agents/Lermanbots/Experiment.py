normalizing when discrete -> continuous; ideally would test with/without normalizing - previously, ExperimentAgent always overrid normalization of action space to [-1, 1]; this was a bug

Agents/Lermanbots/Experiment.py
@@ -46,8 +46,8 @@ class ExperimentAgent(torch.nn.Module):
         self.sample = sample  # Whether to sample inferences variationally as per usual in RL
 
         # TODO can modify actor/critic, don't need this
-        action_spec.discrete = False
-        action_spec.low, action_spec.high = -1, 1  # Note that normalization essential to Classify+RL
+        # action_spec.discrete = False
+        # action_spec.low, action_spec.high = -1, 1  # Note that normalization essential to Classify+RL
 
         # Image augmentation
         self.aug = Utils.instantiate(recipes.aug) or (IntensityAug(0.05) if action_spec.discrete

Blocks/Actors.py
@@ -26,9 +26,7 @@ class EnsemblePiActor(nn.Module):
         self.num_actions = action_spec.discrete_bins or 1  # n
         self.action_dim = math.prod(action_spec.shape) * (1 if stddev_schedule else 2)  # d, or d * 2
 
-        # TODO Can make the else (-1, 1) for Classify + RL to work
-        #  (and classify default can be discrete where All_Qs not normalized so equivalent to traditional/standard)
-        self.low, self.high = (action_spec.low, action_spec.high) if discrete or not action_spec.discrete else (0, 0)
+        self.low, self.high = (action_spec.low, action_spec.high) if discrete or not action_spec.discrete else (-1, 1)
 
         # Standard dev value, max cutoff clip for action sampling
         self.stddev_schedule, self.stddev_clip = stddev_schedule, stddev_clip

Blocks/Critics.py
@@ -24,7 +24,6 @@ class EnsembleQCritic(nn.Module):
         self.num_actions = action_spec.discrete_bins or 1  # n
         self.action_dim = math.prod(action_spec.shape)  # d
 
-        # TODO is this ever called when not discrete?
         self.low, self.high = (action_spec.low, action_spec.high) if discrete else (None,) * 2
 
         # Discrete critic always requires observation
@@ -96,4 +95,4 @@ class EnsembleQCritic(nn.Module):
         action = action.view(action.shape[0], 1, -1, self.action_dim)  # [b, 1, n', d]
 
         return (action - self.low) / (self.high - self.low) * (self.num_actions - 1) if self.low or self.high \
-            else action  # Inverse of normalize -> indices  TODO will self.low/high ever not be set for discrete?
+            else action  # Inverse of normalize -> indices