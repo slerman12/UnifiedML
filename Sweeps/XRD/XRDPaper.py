0412cc70377efc710c9ba3e454b9c0d5bc7526e6
samlerman <agi.init@gmail.com>
2022-09-12 19:36:13 -0400

    bigger mem, universal replays

--- Sweeps/XRD/XRDPaper.py
+++ Sweeps/XRD/XRDPaper.py
@@ -78,8 +78,7 @@ runs.XRD.sweep = [
     +dataset.num_classes=7,230
     train_steps=5e5
     save=true
-    logger.wandb=true
-    lab=true""",
+    logger.wandb=true""",
 
     # Large, MLP
     """task=classify/custom
@@ -98,8 +97,7 @@ runs.XRD.sweep = [
     +dataset.num_classes=7,230
     train_steps=5e5
     save=true
-    logger.wandb=true
-    lab=true""",
+    logger.wandb=true""",
 
     # PS1, CNN
     """task=classify/custom
@@ -143,70 +141,70 @@ runs.XRD.sweep = [
 ]
 
 # Create universal replays
-runs.XRD.sweep = [
-    # Large-Soup, MLP
-    """task=classify/custom
-    Dataset=XRD.XRD
-    Aug=Identity
-    Trunk=Identity
-    Eyes=Identity
-    Predictor=XRD.MLP
-    batch_size=256
-    standardize=false
-    norm=true
-    task_name='Large-Soup_${dataset.num_classes}-Way'
-    experiment='MLP'
-    '+dataset.roots=["/gpfs/fs2/scratch/public/jsalgad2/icsd1.2m_large/","../XRDs/icsd_Datasets/rruff/XY_DIF_noiseAll/"]'
-    +'dataset.train_eval_splits=[1, 0.5]'
-    +dataset.num_classes=7,230
-    train_steps=1
-    save=false
-    num_workers=1
-    logger.wandb=true
-    lab=true""",
-
-    # Large, MLP
-    """task=classify/custom
-    Dataset=XRD.XRD
-    Aug=Identity
-    Trunk=Identity
-    Eyes=Identity
-    Predictor=XRD.MLP
-    batch_size=256
-    standardize=false
-    norm=true
-    task_name='Large_${dataset.num_classes}-Way'
-    experiment='MLP'
-    '+dataset.roots=["/gpfs/fs2/scratch/public/jsalgad2/icsd1.2m_large/","../XRDs/icsd_Datasets/rruff/XY_DIF_noiseAll/"]'
-    +'dataset.train_eval_splits=[1, 0]'
-    +dataset.num_classes=7,230
-    train_steps=1
-    save=false
-    num_workers=1
-    logger.wandb=true
-    lab=true""",
-
-    # PS1, CNN
-    """task=classify/custom
-    Dataset=XRD.XRD
-    Aug=Identity
-    Trunk=Identity
-    Eyes=XRD.CNN
-    Predictor=XRD.Predictor
-    batch_size=256
-    standardize=false
-    norm=true
-    task_name='PS1_${dataset.num_classes}-Way'
-    experiment='CNN'
-    '+dataset.roots=["/gpfs/fs2/scratch/public/jsalgad2/icsd171k_ps1/","../XRDs/icsd_Datasets/rruff/XY_DIF_noiseAll/"]'
-    +'dataset.train_eval_splits=[1, 0]'
-    +dataset.num_classes=7,230
-    train_steps=1
-    save=false
-    num_workers=1
-    logger.wandb=true
-    lab=true""",
-]
+# runs.XRD.sweep = [
+#     # Large-Soup, MLP
+#     """task=classify/custom
+#     Dataset=XRD.XRD
+#     Aug=Identity
+#     Trunk=Identity
+#     Eyes=Identity
+#     Predictor=XRD.MLP
+#     batch_size=256
+#     standardize=false
+#     norm=true
+#     task_name='Large-Soup_${dataset.num_classes}-Way'
+#     experiment='MLP'
+#     '+dataset.roots=["/gpfs/fs2/scratch/public/jsalgad2/icsd1.2m_large/","../XRDs/icsd_Datasets/rruff/XY_DIF_noiseAll/"]'
+#     +'dataset.train_eval_splits=[1, 0.5]'
+#     +dataset.num_classes=7,230
+#     train_steps=1
+#     save=false
+#     num_workers=1
+#     logger.wandb=true
+#     lab=true""",
+#
+#     # Large, MLP
+#     """task=classify/custom
+#     Dataset=XRD.XRD
+#     Aug=Identity
+#     Trunk=Identity
+#     Eyes=Identity
+#     Predictor=XRD.MLP
+#     batch_size=256
+#     standardize=false
+#     norm=true
+#     task_name='Large_${dataset.num_classes}-Way'
+#     experiment='MLP'
+#     '+dataset.roots=["/gpfs/fs2/scratch/public/jsalgad2/icsd1.2m_large/","../XRDs/icsd_Datasets/rruff/XY_DIF_noiseAll/"]'
+#     +'dataset.train_eval_splits=[1, 0]'
+#     +dataset.num_classes=7,230
+#     train_steps=1
+#     save=false
+#     num_workers=1
+#     logger.wandb=true
+#     lab=true""",
+#
+#     # PS1, CNN
+#     """task=classify/custom
+#     Dataset=XRD.XRD
+#     Aug=Identity
+#     Trunk=Identity
+#     Eyes=XRD.CNN
+#     Predictor=XRD.Predictor
+#     batch_size=256
+#     standardize=false
+#     norm=true
+#     task_name='PS1_${dataset.num_classes}-Way'
+#     experiment='CNN'
+#     '+dataset.roots=["/gpfs/fs2/scratch/public/jsalgad2/icsd171k_ps1/","../XRDs/icsd_Datasets/rruff/XY_DIF_noiseAll/"]'
+#     +'dataset.train_eval_splits=[1, 0]'
+#     +dataset.num_classes=7,230
+#     train_steps=1
+#     save=false
+#     num_workers=1
+#     logger.wandb=true
+#     lab=true""",
+# ]
 
 runs.XRD.plots = [
     ['CNN', 'MLP', 'No-Pool-CNN'],
