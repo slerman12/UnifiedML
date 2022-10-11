93ca4bf7566d986e8bf3cd938b15ba86de92cb6c
samlerman <agi.init@gmail.com>
2022-10-11 18:17:37 -0400

    commented

--- Utils.py
+++ Utils.py
@@ -165,7 +165,6 @@ def optimizer_init(params, optim=None, scheduler=None, lr=None, lr_decay_epochs=
     # Optimizer
     optim = len(params) > 0 and (instantiate(optim, params=params, lr=getattr(optim, 'lr', lr)) or lr
                                  and AdamW(params, lr=lr, weight_decay=weight_decay))  # Default
-    print(optim)
 
     # Learning rate scheduler
     scheduler = optim and (instantiate(scheduler, optimizer=optim) or lr_decay_epochs
