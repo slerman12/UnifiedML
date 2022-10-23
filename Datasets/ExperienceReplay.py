2b993e47875d8f4f95c7b25a420cf72b969bdeda
samlerman <agi.init@gmail.com>
2022-10-23 19:08:43 -0400

    trying still for some reason

--- Datasets/ExperienceReplay.py
+++ Datasets/ExperienceReplay.py
@@ -521,6 +521,12 @@ class SharedDict:
         self.mems = {}
         self.specs = specs
 
+        import resource
+        soft_limit, hard_limit = resource.getrlimit(resource.RLIMIT_DATA)
+
+        # Set soft limit
+        resource.setrlimit(resource.RLIMIT_DATA, (hard_limit, hard_limit))
+
     def __setitem__(self, key, value):
         self.start_worker()
 
@@ -584,6 +590,10 @@ class SharedDict:
                     mem = self.mems.setdefault(name, SharedMemory(name=name))
 
                     episode[spec] = np.ndarray(shape, np.float32, buffer=mem.buf)
+
+            if name in self.mems:
+                mem = self.mems[name] if isinstance(self.mems[name], SharedMemory) else self.mems[name].shm
+                mem.close()
 
         return episode
 
