0987cb02b0ffe7b42fb0547c52280b075723aea1
samlerman <agi.init@gmail.com>
2022-10-27 12:37:54 -0400

    ready to merge

--- Sweeps/UnifiedML/ClassifyRL.py
+++ Sweeps/UnifiedML/ClassifyRL.py
@@ -36,6 +36,28 @@ runs = {'UnifiedML': {
         # save_per_steps=100000""",
 
         # Classify + RL: Variational Inference - No EMA
+        # """Agent=Agents.ExperimentAgent
+        # task=classify/mnist,classify/cifar10,classify/tinyimagenet
+        # ema=false
+        # weight_decay=0.01
+        # Eyes=Blocks.Architectures.ResNet18
+        # 'Aug="RandomShiftsAug(4)"'
+        # 'transform="{RandomHorizontalFlip:{}}"'
+        # RL=true
+        # supervise=false
+        # discrete=true,false
+        # +agent.contrastive=true,false
+        # +agent.sample=true
+        # +agent.num_critics=1,2
+        # experiment='Classify+RL+Sample_supervise-${supervise}_discrete-${discrete}_contrastive-${agent.contrastive}_num-critics-${agent.num_critics}_no-EMA'
+        # num_workers=8
+        # plot_per_steps=0
+        # logger.wandb=true
+        # time="12-00:00:00"
+        # reservation_id=20220929
+        # save_per_steps=100000""",
+
+        # Classify + RL: Variational Inference - No EMA - need to redo discrete
         """Agent=Agents.ExperimentAgent 
         task=classify/mnist,classify/cifar10,classify/tinyimagenet
         ema=false 
@@ -45,7 +67,7 @@ runs = {'UnifiedML': {
         'transform="{RandomHorizontalFlip:{}}"'
         RL=true 
         supervise=false
-        discrete=true,false 
+        discrete=true 
         +agent.contrastive=true,false 
         +agent.sample=true
         +agent.num_critics=1,2
