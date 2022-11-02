b6c98b9fdf1d49c5c99f913862a6c7c1e606b978
samlerman <agi.init@gmail.com>
2022-11-02 10:17:52 -0400

    error message

--- Datasets/ExperienceReplay.py
+++ Datasets/ExperienceReplay.py
@@ -50,8 +50,8 @@ class ExperienceReplay:
                         warnings.warn(f'Loading a saved replay of a classify task from a previous online session.'
                                       f'For the standard offline dataset, set replay.path="{standard}" '  # If exists
                                       f'or delete the saved buffer in {path}.')
-            assert len(exists) > 0, f'\nNo existing replay buffer found in path: {path}.\n"generate=true", ' \
-                                    f'"offline=true", & "replay.load=true" all assume presence of a saved replay ' \
+            assert len(exists) > 0, f'\nNo existing replay buffer found in path: {path}.\ngenerate=true, ' \
+                                    f'offline=true, & replay.load=true all assume the presence of a saved replay ' \
                                     f'buffer. \nTry replay.save=true first, then you can try again with one of those 3.'
             self.path = Path(sorted(exists)[-1])
             save = offline or save
