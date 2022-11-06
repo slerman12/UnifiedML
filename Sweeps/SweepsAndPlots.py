cf9e4640b6306b78235b30dcad37f81359f81389
samlerman <agi.init@gmail.com>
2022-11-06 14:38:52 -0500

    xrd paper experiments

--- Sweeps/SweepsAndPlots.py
+++ Sweeps/SweepsAndPlots.py
@@ -32,7 +32,7 @@ runs.Example.bluehive = False
 
 from Sweeps.XRD.XRDPaper import runs
 # from Sweeps.UnifiedML.ClassifyRL import runs
-from Sweeps.UnifiedML.Atari import runs
+# from Sweeps.UnifiedML.Atari import runs
 
 
 runs = convert_to_attr_dict(runs)  # Necessary if runs is defined as a dict!
