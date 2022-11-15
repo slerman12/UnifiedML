7b3f4f3abc803e40a1435b3497afd38d6f8b241c
samlerman <agi.init@gmail.com>
2022-11-15 17:28:26 -0500

    speech commands...

--- Datasets/Suites/Classify.py
+++ Datasets/Suites/Classify.py
@@ -124,6 +124,7 @@ class Classify:
         obs_shape = (1,) * (2 - len(obs_shape)) + obs_shape  # At least 1 channel dim and spatial dim - can comment out
 
         self.obs_spec = {'shape': obs_shape}
+        print(self.obs_spec)
 
         """MOVE TO REPLAY"""
 
