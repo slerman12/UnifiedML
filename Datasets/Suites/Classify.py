233d11a29b7ca26bc8d697132bc709bc7f7b7151
samlerman <agi.init@gmail.com>
2022-12-22 23:40:00 -0500

    debug test

--- Datasets/Suites/Classify.py
+++ Datasets/Suites/Classify.py
@@ -130,9 +130,7 @@ class Classify:
 
         self._batches = iter(self.batches)
 
-        assert False, next(iter(self.batches))
-
-        obs_shape = tuple(next(iter(self.batches))[0].shape[1:])
+        obs_shape = tuple(dataset[0][0].shape[1:])
         obs_shape = (1,) * (2 - len(obs_shape)) + obs_shape  # At least 1 channel dim and spatial dim - can comment out
 
         self.obs_spec = {'shape': obs_shape}
