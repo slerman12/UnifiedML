c84a2edd5b91f8568d24b507c0bb6cbec87ca57b
samlerman <agi.init@gmail.com>
2023-01-19 13:50:37 -0500

    celeba

--- dcgan.py
+++ dcgan.py
@@ -74,13 +74,13 @@ ngpu = 1
 
 # We can use an image folder dataset the way we have it setup.
 # Create the dataset
-dataset = dset.ImageFolder(root=dataroot,
-                           transform=transforms.Compose([
-                               transforms.Resize(image_size),
-                               transforms.CenterCrop(image_size),
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                           ]))
+# dataset = dset.ImageFolder(root=dataroot,
+#                            transform=transforms.Compose([
+#                                transforms.Resize(image_size),
+#                                transforms.CenterCrop(image_size),
+#                                transforms.ToTensor(),
+#                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+#                            ]))
 dataset = torchvision.datasets.celeba.CelebA(root=dataroot,
                                              download=True,
                                              transform=transforms.Compose([
