cd66fe0d21b80436b46366447526f2b8a441b083
samlerman <agi.init@gmail.com>
2023-02-01 17:46:48 -0500

    dcgan

--- dcgan_.py
+++ dcgan_.py
@@ -403,7 +403,7 @@ for epoch in range(num_epochs):
         if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
             with torch.no_grad():
                 fake = netG(fixed_noise).mean.detach().cpu()
-                fake = fake.view(fake.shape[0], *real_cpu.shape)
+                fake = fake.view(fake.shape[0], *real_cpu.shape[1:])
                 print(fake.shape)
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
