98fa7ad62f9f548a1ec1c4bca9043cd292f7227d
sam <slerman@u.rochester.edu>
2023-02-13 22:47:34 -0500

    Perhaps a clearner environment with stream / on_policy

--- Datasets/Environment.py
+++ Datasets/Environment.py
@@ -21,7 +21,7 @@ class Environment:
 
         self.truncate_after = train and truncate_episode_steps or inf  # Truncate episodes shorter (inf if None)
 
-        if not self.disable or self.on_policy:
+        if not self.disable or stream:
             self.env = instantiate(env, task=task, frame_stack=int(stream) or frame_stack, action_repeat=action_repeat,
                                    offline=offline, generate=generate, stream=stream, train=train, seed=seed, **kwargs)
             self.env.reset()
