e4e8ae62c145e2399b782e654f7bffe42b447311
sam <slerman@u.rochester.edu>
2023-02-20 18:04:14 -0500

    dcgan

--- DCGAN.py
+++ DCGAN.py
@@ -11,6 +11,7 @@ import torchvision.utils as vutils
 import numpy as np
 import matplotlib.pyplot as plt
 
+from Blocks.Architectures.Vision.DCGAN import Generator, Discriminator
 from Datasets.Suites._CelebA import CelebA
 from Blocks.Architectures.Vision.CNN import cnn_broadcast
 
@@ -64,121 +65,6 @@ dataset = CelebA(root=dataroot,
 
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=2)
 
-
-# Initializes model weights a la normal
-def weight_init(m):
-    if isinstance(m, (nn.Conv2d, nn.Conv1d)) or isinstance(m, (nn.ConvTranspose2d, nn.ConvTranspose1d)):
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif isinstance(m, (nn.BatchNorm2d, nn.BatchNorm1d)):
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
-
-
-class Generator(nn.Module):
-    def __init__(self, input_shape, hidden_dim=64, output_shape=None):
-        super().__init__()
-
-        self.input_shape, self.output_shape = Utils.to_tuple(input_shape), Utils.to_tuple(output_shape)
-        # Proprioceptive is channel dim
-        self.input_shape = tuple(self.input_shape) + (1,) * (3 - len(self.input_shape))  # Broadcast input to 2D
-
-        in_channels = self.input_shape[0]
-        out_channels = in_channels if self.output_shape is None else self.output_shape[0]
-
-        self.Generator = nn.Sequential(
-            # (hidden_dim * 8) x 4 x 4
-            nn.ConvTranspose2d(in_channels, hidden_dim * 8, 4, bias=False),
-            nn.BatchNorm2d(hidden_dim * 8),
-            nn.ReLU(inplace=True),
-
-            # (hidden_dim * 4) x 8 x 8
-            nn.ConvTranspose2d(hidden_dim * 8, hidden_dim * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(hidden_dim * 4),
-            nn.ReLU(inplace=True),
-
-            # (hidden_dim * 2) x 16 x 16
-            nn.ConvTranspose2d(hidden_dim * 4, hidden_dim * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(hidden_dim * 2),
-            nn.ReLU(inplace=True),
-
-            # hidden_dim x 32 x 32
-            nn.ConvTranspose2d(hidden_dim * 2, hidden_dim, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(hidden_dim),
-            nn.ReLU(inplace=True),
-
-            # out_channels x 64 x 64
-            nn.ConvTranspose2d(hidden_dim, out_channels, 4, 2, 1, bias=False),
-            # nn.Identity() if self.output_shape is None else nn.AdaptiveAvgPool2d(self.output_shape[1:])  # Adapts scale
-            nn.Tanh()  # TODO added here
-        )
-
-        self.apply(weight_init)
-
-    def repr_shape(self, *_):
-        return Utils.repr_shape(_, self.Generator)  # cnn_feature_shape doesn't support pre-computing ConvTranspose2d
-
-    def forward(self, *x):
-        # Concatenate inputs along channels assuming dimensions allow, broadcast across many possibilities
-        lead_shape, x = cnn_broadcast(self.input_shape, x)
-
-        x = self.Generator(x)
-
-        # Restore leading dims
-        out = x.view(*lead_shape, *(self.output_shape or x.shape[1:]))
-        return out
-
-
-# TODO uncommnet the adaptive pools
-class Discriminator(nn.Module):
-    def __init__(self, input_shape, hidden_dim=64, output_shape=None):
-        super().__init__()
-
-        self.input_shape, self.output_shape = Utils.to_tuple(input_shape), Utils.to_tuple(output_shape)
-
-        in_channels = self.input_shape[0]
-
-        self.Discriminator = nn.Sequential(
-            # hidden_dim x 32 x 32
-            # nn.AdaptiveAvgPool2d(64),  # Adapts from different scales  TODO Does averaging cause weird distortion?
-            nn.Conv2d(in_channels, hidden_dim, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # (hidden_dim * 2) x 16 x 16
-            nn.Conv2d(hidden_dim, hidden_dim * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(hidden_dim * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # (hidden_dim * 4) x 8 x 8
-            nn.Conv2d(hidden_dim * 2, hidden_dim * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(hidden_dim * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # (hidden_dim * 8) x 4 x 4
-            nn.Conv2d(hidden_dim * 4, hidden_dim * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(hidden_dim * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # 1 x 1 x 1
-            nn.Conv2d(hidden_dim * 8, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()  # TODO Using Sigmoid with MSE breaks for some reason!
-        )
-
-        self.apply(weight_init)
-
-    def repr_shape(self, *_):
-        return Utils.cnn_feature_shape(_, self.Discriminator)
-
-    def forward(self, *x):
-        # Concatenate inputs along channels assuming dimensions allow, broadcast across many possibilities
-        lead_shape, x = cnn_broadcast(self.input_shape, x)
-
-        x = self.Discriminator(x)
-
-        # Restore leading dims
-        out = x.view(*lead_shape, *(self.output_shape or x.shape[1:]))
-        return out
-
-
 obs_spec = AttrDict({'shape': [channels, 64, 64], 'mean': 0.5, 'stddev': 0.5, 'low': 0, 'high': 1})
 action_spec = AttrDict({'shape': obs_spec.shape, 'discrete_bins': None, 'low': -1, 'high': 1, 'discrete': False})
 
@@ -186,8 +72,7 @@ encoder = CNNEncoder(obs_spec, standardize=False, Eyes=nn.Identity)
 actor = EnsemblePiActor(encoder.repr_shape, 100, -1, action_spec, trunk=Utils.Rand, Pi_head=Generator, ensemble_size=1,
                         lr=lr, optim={'_target_': 'Adam', 'betas': [beta1, 0.999]}).to(device)
 critic = EnsembleQCritic(encoder.repr_shape, 100, -1, action_spec, Q_head=Discriminator, ensemble_size=1,
-                         ignore_obs=True, lr=lr,
-                         optim={'_target_': 'Adam', 'betas': [beta1, 0.999]}).to(device)
+                         ignore_obs=True, lr=lr, optim={'_target_': 'Adam', 'betas': [beta1, 0.999]}).to(device)
 
 
 optimizerG = optim.Adam(actor.parameters(), lr=lr, betas=(beta1, 0.999))
