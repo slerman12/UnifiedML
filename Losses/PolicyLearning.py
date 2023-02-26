2148aa706b383d01189202944de2715261695878
sam <slerman@u.rochester.edu>
2023-02-26 11:30:41 -0500

    bce but with log in PG

--- Losses/PolicyLearning.py
+++ Losses/PolicyLearning.py
@@ -2,6 +2,8 @@
 #
 # This source code is licensed under the MIT license found in the
 # MIT_LICENSE file in the root directory of this source tree.
+import torch
+from torch.nn.functional import binary_cross_entropy
 
 
 def deepPolicyGradient(actor, critic, obs, step, logs=None):
@@ -12,7 +14,8 @@ def deepPolicyGradient(actor, critic, obs, step, logs=None):
     Qs = critic(obs, action)
     q, _ = Qs.min(1)  # Min-reduced ensemble
 
-    # if critic.binary:
+    if critic.binary:
+        q = -binary_cross_entropy(q, torch.ones_like(q))
     #     q = q.log()  # For numerical stability of maximizing Bernoulli variables
 
     policy_loss = -q.mean()  # Policy gradient ascent
