ecc7bfe204a64c489808787bb87e64ec60e0094f
sam <slerman@u.rochester.edu>
2023-02-26 09:58:39 -0500

    dcgan with dropot

--- Discriminator.py
+++ Discriminator.py
@@ -11,21 +11,25 @@ class Discriminator(nn.Module):
             # 64 x 32 x 32
             nn.Conv2d(3, 64, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5),
 
             # (64 * 2) x 16 x 16
             nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
             nn.InstanceNorm2d(64 * 2),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5),
 
             # (64 * 4) x 8 x 8
             nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
             nn.InstanceNorm2d(64 * 4),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5),
 
             # (64 * 8) x 4 x 4
             nn.Conv2d(64 * 4, 64 * 8, 4, 2, 1, bias=False),
             nn.InstanceNorm2d(64 * 8),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5),
 
             # 1 x 1 x 1
             nn.Conv2d(64 * 8, 1, 4, 1, 0, bias=False),
