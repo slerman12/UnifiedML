9dc4542c8d41e5fd9831281a1715859e56784c1e
sam <slerman@u.rochester.edu>
2023-03-17 15:26:51 -0400

    Creator template

--- Blocks/Creator.py
+++ Blocks/Creator.py
@@ -16,8 +16,8 @@ class Creator(torch.nn.Module):
     """
     Selects over action spaces based on probabilities and over ensembles based on Critic-evaluated "goodness"
     """
-    def __init__(self, action_spec, critic, explore=None, exploit=None, second_sample=False,
-                 discrete=False, stddev_schedule=1, stddev_clip=torch.inf, optim=None, scheduler=None,
+    def __init__(self, action_spec, critic=None, explore=None, exploit=None, second_sample=False,
+                 discrete=False, temp_schedule=1, stddev_clip=torch.inf, optim=None, scheduler=None,
                  lr=None, lr_decay_epochs=None, weight_decay=None, ema_decay=None):
         super().__init__()
 
@@ -30,8 +30,8 @@ class Creator(torch.nn.Module):
         self.action = self.first_sample = None
 
         # Exploration / Exploitation policies
-        self.Explore = MonteCarloPolicy(discrete, stddev_schedule, stddev_clip, self.low, self.high, critic)
-        self.Exploit = BestPolicy(discrete, critic)
+        self.Explore = MonteCarloPolicy(discrete, temp_schedule, stddev_clip, self.low, self.high, critic)
+        self.Exploit = ArgmaxPolicy(discrete, self.low, self.high, critic)
 
         # Initialize model optimizer + EMA
         self.optim, self.scheduler = Utils.optimizer_init(self.parameters(), optim, scheduler,
@@ -40,101 +40,183 @@ class Creator(torch.nn.Module):
             self.ema_decay = ema_decay
             self.ema = copy.deepcopy(self).requires_grad_(False)
 
-    def forward(self, action, explore_rate):
+    def forward(self, action, explore_rate, step):
         # Initialize policies
-        self.Explore(action, explore_rate)
+        self.Explore(action, explore_rate, step)
         self.Exploit(action)
 
         return self  # Return self as an initialized distribution
 
-    def probs(self, action, Qs=None):
-        return self.Explore.probs(action, Qs) if hasattr(self.Explore, 'probs') \
-            else torch.ones(1, device=action.device)
+    def probs(self, action, q=None, as_ensemble=True):
+        return self.Explore.probs(action, q, as_ensemble)
 
-    def sample(self, sample_shape=None):
-        sample = self.Explore.sample(sample_shape)
+    def sample(self, sample_shape=None, detach=True):
+        action = self.first_sample = self.Explore.sample(sample_shape) if detach else self.Explore.rsample(sample_shape)
 
-        # Normalize Discrete Action -> [low, high]
-        if self.discrete and self.low is not None and self.high is not None:
-            sample = sample / (self.action_dim - 1) * (self.high - self.low) + self.low
+        if self.second_sample and not self.discrete:
+            pass  # TODO Second sample
 
-        return sample
+        return action
+
+    def rsample(self, sample_shape=None):
+        return self.sample(sample_shape, detach=False)
 
     @property
     def best(self):
         action = self.Exploit.sample()
 
-        # Normalize Discrete Action -> [low, high]
-        if self.discrete and self.low is not None and self.high is not None:
-            action = action / (self.action_dim - 1) * (self.high - self.low) + self.low
-
         return action  # Best action
+
+    # TODO goodness (instead of logits), entropy (for SAC), log_probs
+    #   Perhaps Creator should do parallel inference end-to-end
 
 
 class MonteCarloPolicy(torch.nn.Module):
     """
-    RL policy
+    RL policy - samples action across action space and ensemble space
 
     A policy must contain a sample(·) method
-            sample: sample_shape -> action(s)
+            sample: sample_shape (default None) -> action(s)
                     If sample_shape is None, the policy returns exactly 1 action
-                    Otherwise, the policy returns a factor N of the action ensemble size actions
+                    Otherwise, the policy returns a factor N=prod(sample_shape) of the actor ensemble size actions
     For use in Q-learning, a policy should include a probs(·) method
             probs: action, Qs -> probability
+    Any post-sampling, pre-ensemble-reduction operations may be specified by ActionExtractor
     """
-    def __init__(self, discrete, stddev_schedule, stddev_clip, low, high, critic):
+    def __init__(self, discrete=True, temp_schedule=1, stddev_clip=None, low=None, high=None, critic=None):
         super().__init__()
 
         self.Pi = None
 
         self.discrete = discrete
 
-        # Standard dev value, max cutoff clip for action sampling
-        self.stddev_schedule, self.stddev_clip = stddev_schedule, stddev_clip
+        # Entropy value, max cutoff clip for action sampling
+        self.temp_schedule, self.stddev_clip = temp_schedule, stddev_clip
 
         self.low, self.high = low, high
 
+        self.step = self.obs = None
         self.critic = critic
 
-    def forward(self, mean, stddev):
-        if self.discrete:  # TODO Maybe return these, pass all probs/sampling/even Qs to Distributions
-            logits, ind = mean.min(1)  # Min-reduced ensemble [b, n, d]
-            stddev = Utils.gather(stddev, ind.unsqueeze(1), 1, 1).squeeze(1)  # Min-reduced ensemble stand dev [b, n, d]
+    def forward(self, action, explore_rate, step, obs):
+        if self.discrete:
+            # Pessimism
+            logits, ind = action.min(1)  # Min-reduced ensemble [b, n, d]
+            stddev = Utils.gather(explore_rate, ind.unsqueeze(1), 1, 1).squeeze(1)  # Min-reduced ensemble std [b, n, d]
 
             self.Pi = NormalizedCategorical(logits=logits, low=self.low, high=self.high, temp=stddev, dim=-2)
         else:
-            self.Pi = TruncatedNormal(mean, stddev, low=self.low, high=self.high, stddev_clip=self.stddev_clip)
+            self.Pi = TruncatedNormal(action, explore_rate, low=self.low, high=self.high, stddev_clip=self.stddev_clip)
 
-    def probs(self, action, Qs=None):
-        pass
+        # For critic-based ensemble reduction
+        if self.critic is not None:
+            self.step = step
+            self.obs = obs
 
-    def sample(self, sample_shape=None):  # TODO rsample!!
-        action = self.Pi.sample(sample_shape or 1)  # TODO critic if ensemble > 1 and sample_shape is None
+    def probs(self, action, q=None, as_ensemble=True):
+        probs = self.Pi.probs(action)
+
+        if not as_ensemble:
+            return probs
+
+        # Ensembles are reduced based on Q-value
+        if not (self.critic is None or self.discrete) and q is None and action.shape[1] > 1:
+            # Pessimistic Q-values per action
+            q = self.critic(self.obs, action).min(1)[0]  # Min-reduced critic ensemble
+
+        # Ensemble-wise probability
+        if q is not None:
+            q_norm = q - q.max(-1, keepdim=True)[0]  # Normalized ensemble-wise
+
+            # Categorical distribution based on Q-value
+            temp = Utils.schedule(self.temp_schedule, self.step)  # Softmax temperature / "entropy"
+            probs *= (q_norm / temp).softmax(-1)  # Probabilities
+
+        return probs
+
+    def sample(self, sample_shape=None, detach=True):
+        action = self.Pi.sample(sample_shape or 1) if detach else self.Pi.rsample(sample_shape or 1)
+        # action = self.ActionExtractor(action)  # TODO
+
+        # Reduce ensemble
+        if self.critic is not None and sample_shape is None and action.shape[1] > 1 and not self.discrete:
+            # Pessimistic Q-values per action
+            Qs = self.critic(self.obs, action)
+            q = Qs.min(1)[0]  # Min-reduced critic ensemble
+
+            # Normalize
+            q -= q.max(-1, keepdim=True)[0]
+
+            # Softmax temperature
+            temp = Utils.schedule(self.temp_schedule, self.step) if self.step else 1
+
+            # Categorical dist
+            Psi = torch.distributions.Categorical(logits=q / temp)
+
+            action = Utils.gather(action, Psi.sample().unsqueeze(-1), 1).squeeze(1)
 
         return action
 
+    def rsample(self, sample_shape=None):
+        return self.sample(sample_shape, detach=False)
 
-class BestPolicy(torch.nn.Module):
+
+class ArgmaxPolicy(torch.nn.Module):
     """
-    RL policy
+    RL policy - samples action across action space and ensemble space
 
     A policy must contain a sample(·) method
-            sample: sample_shape -> action(s)
+            sample: sample_shape (default None) -> action(s)
                     If sample_shape is None, the policy returns exactly 1 action
-                    Otherwise, the policy returns a factor N of the action ensemble size actions
+                    Otherwise, the policy returns a factor N=prod(sample_shape) of the actor ensemble size actions
     For use in Q-learning, a policy should include a probs(·) method
             probs: action, Qs -> probability
+    Any post-sampling, pre-ensemble-reduction operations may be specified by ActionExtractor
     """
-    def __init__(self, discrete, critic):
+    def __init__(self, discrete=True, low=None, high=None, critic=None):
         super().__init__()
 
-        self.best = None
         self.discrete = discrete
+        self.low, self.high = low, high
+        self.best = self.obs = None
         self.critic = critic
 
-    def forward(self, actions):
+    def forward(self, action, explore_rate=None, step=None, obs=None):
         # Argmax for Discrete, Mean for Continuous
-        self.best = actions.argmax(-1, keepdim=True).transpose(-1, -2) if self.discrete else actions
+        self.best = action.argmax(-1, keepdim=True).transpose(-1, -2) if self.discrete else action
+
+        # Normalize Discrete Action -> [low, high]
+        if self.discrete and self.low is not None and self.high is not None:
+            self.best = self.best / (action.shape[-1] - 1) * (self.high - self.low) + self.low
+
+        # For critic-based ensemble reduction
+        if self.critic is not None:
+            self.obs = obs
+
+    def probs(self, action, q=None, as_ensemble=True):
+        # Absolute Determinism
+        return torch.ones([1], device=action.device).expand(len(action), 1)
 
     def sample(self, sample_shape=None):
-        return self.best  # TODO critic if ensemble > 1 and sample_shape is None
+        action = self.best  # Note: sample_shape is at most (1,)
+        # action = self.ActionExtractor(action)  # TODO
+
+        # Reduce ensemble
+        if self.critic is not None and sample_shape is None and action.shape[1] > 1 and not self.discrete:
+            # Q-values per action
+            Qs = self.critic(self.obs, action)
+            q = Qs.mean(1)  # Mean-reduced ensemble
+
+            # Normalize
+            q -= q.max(-1, keepdim=True)[0]
+
+            # Highest Q-value
+            _, best_ind = q.max(-1)
+
+            # Action corresponding to highest Q-value
+            action = Utils.gather(action, best_ind.unsqueeze(-1), 1).squeeze(1)
+
+        return action
+
+    def rsample(self, sample_shape=None):
+        return self.sample(sample_shape)  # Note: Not differentiable
