a559df2eb73651f699b6a6292800b924b36a961a
sam <slerman@u.rochester.edu>
2023-03-18 21:38:26 -0400

    creaotor

--- Blocks/Creator.py
+++ Blocks/Creator.py
@@ -15,10 +15,9 @@ import Utils
 
 class Creator(torch.nn.Module):
     """
-    Selects over action spaces based on probabilities and over ensembles based on Critic-evaluated "goodness"
-    A distribution consisting of an exploration policy and an exploitation policy
+    Sampling distribution and probabilistic measures for action spaces and ensembles.
     """
-    def __init__(self, action_spec, critic=None, ActionExtractor=None, second_sample=False,
+    def __init__(self, action_spec, critic=None, ActionExtractor=None, sample_discrete_as_discrete=True,
                  discrete=False, temp_schedule=1, stddev_clip=torch.inf, optim=None, scheduler=None,
                  lr=None, lr_decay_epochs=None, weight_decay=None, ema_decay=None):
         super().__init__()
@@ -31,11 +30,13 @@ class Creator(torch.nn.Module):
         # Entropy value, max cutoff clip for action sampling
         self.temp_schedule, self.stddev_clip = temp_schedule, stddev_clip
 
-        self.ActionExtractor = Utils.instantiate(ActionExtractor) or nn.Identity()
+        # A mapping applied after sampling but prior to ensemble reduction
+        self.ActionExtractor = Utils.instantiate(ActionExtractor, in_shape=self.action_dim) or nn.Identity()
 
-        self.second_sample = second_sample
+        # Re-sample a discrete action from a sampled continuous categorical distribution
+        self.sample_discrete_as_discrete = action_spec.discrete and not self.discrete and sample_discrete_as_discrete
 
-        self.Pi = self.first_sample = self.action = self.step = self.obs = None
+        self.Pi = self.first_sample = self.action = self.best_action = self.step = self.obs = None
         self.critic = critic
 
         # Initialize model optimizer + EMA
@@ -45,6 +46,7 @@ class Creator(torch.nn.Module):
             self.ema_decay = ema_decay
             self.ema = copy.deepcopy(self).requires_grad_(False)
 
+    # Sets distribution
     def forward(self, action, explore_rate, step, obs):
         self.action = action
 
@@ -61,51 +63,66 @@ class Creator(torch.nn.Module):
             self.step = step
             self.obs = obs
 
-    def probs(self, action, q=None, as_ensemble=True):
+        self.best_action = None
+
+        # Returns itself as policy
+        return self
+
+    def probs(self, action, judgement=None, as_ensemble=True):
+        # Individual action probability
         probs = self.Pi.probs(action)
 
-        # Ensemble-wise probabilities if actions are additionally sampled from ensembles
+        # If action is an ensemble, multiply probability of sampling action from the ensemble
         if as_ensemble and not self.discrete and self.critic is not None and action.shape[1] > 1:
-            if q is None:
-                q, _ = self.critic.judgement(self.critic(self.obs, action))  # Reduced critic-ensemble Q-values
+            if judgement is None:
+                judgement, _ = self.critic.judgement(self.critic(self.obs, action))  # Reduced critic-ensemble Q-values
 
-            if q is not None:
-                q_norm = q - q.max(-1, keepdim=True)[0]  # Normalize ensemble-wise
+            if judgement is not None:
+                judgement = judgement - judgement.max(-1, keepdim=True)[0]  # Normalize critic's judgement ensemble-wise
 
-                # Categorical distribution based on Q-value
+                # Categorical distribution based on critic's judgement
                 temp = Utils.schedule(self.temp_schedule, self.step)  # Softmax temperature / "entropy"
-                probs *= (q_norm / temp).softmax(-1)  # Actor-ensemble-wise probabilities
+                probs *= (judgement / temp).softmax(-1)  # Actor-ensemble-wise probabilities
+
+        # Multiply probability of sampling a discrete action from a continuous-ized categorical distribution
+        if self.sample_discrete_as_discrete:
+            pass  # TODO
 
         return probs
 
-    # Explore
+    # Exploration policy
     def sample(self, sample_shape=None, detach=True):
+        # Sample
         action = self.ActionExtractor(self.Pi.sample(sample_shape or 1) if detach
                                       else self.Pi.rsample(sample_shape or 1))
 
-        # Reduce ensemble
+        # Reduce Actor ensemble
         if self.critic is not None and sample_shape is None and action.shape[1] > 1 and not self.discrete:
             Qs = self.critic(self.obs, action)
-            q, _ = self.critic.judgement(Qs)  # Reduce ensemble
+            judgement, _ = self.critic.judgement(Qs)  # Reduce critic ensemble (e.g. Pessimism <--> Min-reduce)
 
             # Normalize
-            q -= q.max(-1, keepdim=True)[0]
+            judgement -= judgement.max(-1, keepdim=True)[0]
 
             # Softmax temperature
             temp = Utils.schedule(self.temp_schedule, self.step) if self.step else 1
 
             # Categorical dist
-            Psi = torch.distributions.Categorical(logits=q / temp)
+            Psi = torch.distributions.Categorical(logits=judgement / temp)
 
+            # Sample again ensemble-wise
             action = Utils.gather(action, Psi.sample().unsqueeze(-1), 1).squeeze(1)
+
+        # Can sample again from a continuous categorical distribution to get a discrete action if action continuous-ized
+        if self.sample_discrete_as_discrete:
+            pass  # TODO
 
         return action
 
-    # Differentiable Explore
     def rsample(self, sample_shape=None):
-        return self.sample(sample_shape, detach=False)
+        return self.sample(sample_shape, detach=False)  # Differentiable explore
 
-    # Exploit
+    # Exploitation policy
     @property
     def best(self):
         # Absolute Determinism
@@ -123,7 +140,7 @@ class Creator(torch.nn.Module):
         if self.critic is not None and not self.discrete and action.shape[1] > 1:
             # Q-values per action
             Qs = self.critic(self.obs, action)
-            q = Qs.mean(1)  # Mean-reduced ensemble
+            q = Qs.mean(1)  # Mean-reduced ensemble. Note: Not using self.critic.judgement(Qs)
 
             # Normalize
             q -= q.max(-1, keepdim=True)[0]
@@ -133,5 +150,11 @@ class Creator(torch.nn.Module):
 
             # Action corresponding to highest Q-value
             action = Utils.gather(action, best_ind.unsqueeze(-1), 1).squeeze(1)
+
+        # Can Argmax again from a continuous categorical distribution to get a discrete action if action continuous-ized
+        if self.sample_discrete_as_discrete:
+            pass  # TODO
+
+        self.best_action = action
 
         return action
