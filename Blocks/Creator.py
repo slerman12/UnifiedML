f95dd246ea0c636f989c30f2885c5157c28aaa03
sam <slerman@u.rochester.edu>
2023-03-20 09:39:23 -0400

    creator

--- Blocks/Creator.py
+++ Blocks/Creator.py
@@ -16,13 +16,17 @@ import Utils
 
 class Creator(torch.nn.Module):
     """Creates a policy distribution for sampling actions and ensembles and computing probabilistic measures."""
-    def __init__(self, action_spec, policy=None, ActionExtractor=None,
+    def __init__(self, action_spec, policy=None, ActionExtractor=None, discrete=False, stddev_clip=torch.inf,
                  optim=None, scheduler=None, lr=None, lr_decay_epochs=None, weight_decay=None, ema_decay=None):
         super().__init__()
 
-        self.policy = policy  # Exploration and exploitation policy recipe
-
+        self.discrete = discrete
         self.action_spec = action_spec
+
+        # Max cutoff clip for continuous-action sampling
+        self.stddev_clip = stddev_clip  # TODO Make this default/automatic. Pass in schedule for log_prob of 2nd sample.
+
+        self.policy = policy  # Exploration and exploitation policy recipe
 
         # A mapping that can be applied after or concurrently with action sampling
         self.ActionExtractor = Utils.instantiate(ActionExtractor, input_shape=math.prod(self.action_spec.shape)
@@ -35,13 +39,12 @@ class Creator(torch.nn.Module):
             self.ema_decay = ema_decay
             self.ema = copy.deepcopy(self).requires_grad_(False)
 
-    def Pi(self, mean, stddev, discrete=False, stddev_clip=torch.inf):  # TODO Rename: action, explore_rate
+    def Pi(self, mean, stddev, step=1):  # TODO Rename maybe
         # Optionally create policy from recipe
-        policy = Utils.instantiate(self.policy, mean=mean, stddev=stddev, action_spec=self.action_spec,
-                                   ActionExtractor=self.ActionExtractor, discrete=discrete, stddev_clip=stddev_clip)
-
-        # Return policy distribution, default: MonteCarlo
-        return policy or MonteCarlo(mean, stddev, self.action_spec, self.ActionExtractor, discrete, stddev_clip)
+        return Utils.instantiate(self.policy, mean=mean, stddev=stddev, action_spec=self.action_spec, step=step,
+                                 ActionExtractor=self.ActionExtractor, discrete=self.discrete,
+                                 stddev_clip=self.stddev_clip) or \
+            MonteCarlo(mean, stddev, self.action_spec, self.ActionExtractor, self.discrete, self.stddev_clip)  # Default
 
 
 # Policy
@@ -54,21 +57,36 @@ class MonteCarlo(torch.nn.Module):
 
         self.low, self.high = action_spec.low, action_spec.high
 
+        # Max cutoff clip for action sampling
+        self.stddev_clip = stddev_clip
+
         # Policy
         if self.discrete:
-            # Pessimistic Q-values per action
-            logits, ind = self.action.min(1)  # Min-reduced ensemble [b, n, d]
-            stddev = Utils.gather(stddev, ind.unsqueeze(1), 1, 1).squeeze(1)  # Min-reduced ensemble std [b, n, d]
-
-            self.Psi = NormalizedCategorical(logits=logits, low=self.low, high=self.high, temp=stddev, dim=-2)
             self.All_Qs = mean  # [b, e, n, d]
-            self.logits = self.Psi.logits  # [b, e, n, d] After log-sum-exp trick
-            self.normalize = self.Psi.normalize  # Normalize -> [low, high]
         else:
-            self.Psi = TruncatedNormal(self.action, stddev, low=self.low, high=self.high, stddev_clip=stddev_clip)
             self.mean = mean  # [b, e, n, d]
 
+        self.stddev = stddev
+
         self.ActionExtractor = ActionExtractor
+
+    # Policy
+    @cached_property
+    def Psi(self):
+        if self.discrete:
+            # Pessimistic Q-values per action
+            logits, ind = self.All_Qs.min(1)  # Min-reduced ensemble [b, n, d]
+            stddev = Utils.gather(self.stddev, ind.unsqueeze(1), 1, 1).squeeze(1)  # Min-reduced ensemble std [b, n, d]
+
+            try:
+                return NormalizedCategorical(logits=logits, low=self.low, high=self.high, temp=stddev, dim=-2)
+            except ValueError:
+                # M1 Mac MPS can sometimes throw ValueError on log-sum-exp trick
+                Psi = NormalizedCategorical(logits=logits.to('cpu'), low=self.low, high=self.high, temp=stddev, dim=-2)
+                Psi.logits = Psi.logits.to(logits.device)
+                return Psi
+        else:
+            return TruncatedNormal(self.mean, self.stddev, low=self.low, high=self.high, stddev_clip=self.stddev_clip)
 
     def log_prob(self, action):
         # Individual action probability
@@ -92,7 +110,7 @@ class MonteCarlo(torch.nn.Module):
             action = self.ActionExtractor(action)
 
         if sample_shape is None and action.shape[1] > 1 and not self.discrete:
-            pass  # TODO Uniform sample (index via rand int)
+            return action[:, torch.randint(action.shape[1], [])]  # Uniform sample across ensemble
 
         # TODO 2nd sample
 
@@ -107,5 +125,5 @@ class MonteCarlo(torch.nn.Module):
         # Absolute Determinism
 
         # Argmax for discrete, extract action for continuous
-        return self.normalize(self.logits.argmax(-1, keepdim=True).transpose(-1, -2)) if self.discrete \
+        return self.Psi.normalize(self.Psi.logits.argmax(-1, keepdim=True).transpose(-1, -2)) if self.discrete \
             else self.ActionExtractor(self.mean)
