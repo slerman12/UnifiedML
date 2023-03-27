4c4f7e370bcf626012132102a982c89d9a29d143
sam <slerman@u.rochester.edu>
2023-03-26 23:08:57 -0400

    testing additive gan

--- Losses/QLearning.py
+++ Losses/QLearning.py
@@ -48,7 +48,7 @@ def ensembleQLearning(critic, actor, obs, action, reward, discount=1, next_obs=N
     criterion = binary_cross_entropy if critic.binary else mse_loss
 
     # Temporal difference (TD) error
-    q_loss = criterion(Qs, target_Q.unsqueeze(1).expand_as(Qs))
+    q_loss = criterion(Qs.float(), target_Q.unsqueeze(1).float().expand_as(Qs))
 
     if logs is not None:
         logs['temporal_difference_error'] = q_loss
