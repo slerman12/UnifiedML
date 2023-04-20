dfbaa1e563a0017a832af19ffa2c15bda7a9901a
sam <slerman@u.rochester.edu>
2023-04-20 14:21:31 -0400

    Launcher

--- __init__.py
+++ __init__.py
@@ -42,8 +42,9 @@ import inspect
 def import_paths():
     UnifiedML = os.path.dirname(__file__)
     app = '/'.join(str(inspect.stack()[-1][1]).split('/')[:-1])
+    print(UnifiedML, app)
 
-    sys.path.extend([app, UnifiedML])  # Imports UnifiedML paths and the paths of the launching app
+    sys.path.extend([UnifiedML, app])  # Imports UnifiedML paths and the paths of the launching app
 
     if os.path.exists(app + '/Hyperparams'):
         sys.argv.extend(['-cd', app + '/Hyperparams'])  # Adds an app's Hyperparams dir to Hydra's .yaml search path
