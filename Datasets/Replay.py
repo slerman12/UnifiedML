131da3d635bb946e251f891c3da624f539294c52
sam <slerman@u.rochester.edu>
2023-05-29 22:09:06 -0400

    replay

--- Datasets/Replay.py
+++ Datasets/Replay.py
@@ -3,28 +3,41 @@ import timeit
 import torch
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
+from torch import multiprocessing as mp
 
 
 class SimpleDataset(Dataset):
-    def __init__(self, data, device):
+    def __init__(self, data, device, queue):
         # We split into a list since it is faster to dataload (fair comparison vs others)
         self.data = data
         self.device = device
+        self.queue = queue
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return self.data[idx].to(self.device)
+        data = self.data[idx]
+        if self.device == 'cuda':
+            data.to(self.device).share_memory_()
+        self.queue.put(data)
 
 
 def run():
+    prefetch_queue = mp.Queue()
     d = torch.rand(1000, 50, 2)
-    dataset = SimpleDataset(d, 'cuda')
-    dataloader = DataLoader(dataset, batch_size=32, num_workers=8, collate_fn=torch.stack)
+    dataset = SimpleDataset(d, 'mps', prefetch_queue)
+    dataloader = DataLoader(dataset, batch_size=32, num_workers=8, collate_fn=none)
     next(iter(dataloader))
+    while not prefetch_queue.empty():
+        print(prefetch_queue.get().shape)
+
+
+def none(*x, **y):
+    return
 
 
 if __name__ == '__main__':
-    torch.multiprocessing.set_start_method('fork')
-    print(timeit.timeit(run, number=1))
+    run()
+    # torch.multiprocessing.set_start_method('fork')
+    # print(timeit.timeit(run, number=1))
