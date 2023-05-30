b45cfa7530fe8869e4546ffeca06264439ff5766
sam <slerman@u.rochester.edu>
2023-05-30 13:59:41 -0400

    MemoryV11?

--- Datasets/MemoryV11.py
+++ Datasets/MemoryV11.py
@@ -43,7 +43,7 @@ class Memory:
         self.num_batches_deleted = torch.zeros([], dtype=torch.int64).share_memory_()
         self.num_batches = self.num_experiences = self.num_experiences_mmapped = self.num_episodes_deleted = 0
 
-        # atexit.register(self.cleanup)
+        atexit.register(self.cleanup)
 
         _, hard_limit = resource.getrlimit(resource.RLIMIT_NOFILE)  # Shared memory can create a lot of file descriptors
         resource.setrlimit(resource.RLIMIT_NOFILE, (hard_limit, hard_limit))  # Increase soft limit to hard limit
