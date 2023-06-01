6f55862aa7ac230a03f5eb81c0a98842120c19ea
sam <slerman@u.rochester.edu>
2023-06-01 08:45:29 -0400

    Parallelize

--- Utils.py
+++ Utils.py
@@ -765,9 +765,10 @@ class Parallelize(nn.Module):
 
     def forward(self, *args):
         if len(self.replicas) > 1:
-            args = tuple(arg.split(len(self.devices)) for arg in args)
-        print(len(args), args[0][0].shape)
+            args = tuple(arg[device]
+                         for arg in tuple(arg.split(len(self.devices)) for arg in args)
+                         for device in range(len(self.devices)))
 
-        return torch.concat([module(*args[:, i]).to(self.devices[0])
+        return torch.concat([module(*args[i]).to(self.devices[0])
                              for i, module in enumerate(self.replicas)]) if len(self.replicas) > 1 \
             else self.replicas[0](*args)
