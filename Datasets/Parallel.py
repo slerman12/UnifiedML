a33237f329e47282bbc8b1af3b83207e3e144442
sam <slerman@u.rochester.edu>
2023-06-01 12:33:23 -0400

    [Parallelize

--- Datasets/Parallel.py
+++ Datasets/Parallel.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 
-class Parallelize(nn.Module):  # Nope.
+class Parallelize(nn.Module):
     def __init__(self, module):
         super().__init__()
 
@@ -11,7 +11,7 @@ class Parallelize(nn.Module):  # Nope.
         self.replicas = nn.ModuleList([module.to(torch._utils._get_device_index(device, True))
                                        for device in self.devices] if self.devices else [module])
 
-        self.streams = [torch.cuda.current_stream(device) for device in self.devices]
+        self.streams = [torch.cuda.current_stream(device) for device in self.devices]  # Note: Can't be pickled for EMA
 
         print(f'Parallelizing across {len(self.replicas) if self.devices else 0} cuda devices.')
 
@@ -26,20 +26,13 @@ class Parallelize(nn.Module):  # Nope.
                 split = [quotient] * (len(self.devices) + bool(remainder))
                 split[-1] += remainder
 
-                splits.append(split)
-
-            splits = [torch.split(arg, split) for arg, split in zip(args, splits)]
-            args = [[split[device] for split in splits] for device in range(len(self.devices))]
+                splits.append(torch.split(arg, split))
 
             outs = []
 
-            torch.cuda.synchronize()
-
             for i, module in enumerate(self.replicas):
                 with torch.cuda.stream(self.streams[i]):
-                    outs.append(module(*args[i]).to(self.devices[0]))
-
-            torch.cuda.synchronize()
+                    outs.append(module(*[split[i] for split in splits]).to(self.devices[0]))
 
             outs = torch.concat(outs)
         else:
